c3f3d2ca27f7f62747feadf866dcb0100a10ea32
ZZAMBAs <rksidksrksi@naver.com>
2024-02-06 12:47:44 +0900

    initial commit

---
+++ src/main/java/org/prgms/locomocoserver/LocomocoServerApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.locomocoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocomocoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocomocoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prgms/locomocoserver/LocomocoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgms.locomocoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocomocoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
