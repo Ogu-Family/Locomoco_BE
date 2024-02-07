f1f6bd3ba5e1fb4ec1530de0f9d3a1547cb40165
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-07 10:17:14 +0900

    feat: user service, controller 기본 구성

---
+++ src/main/java/org/prgms/locomocoserver/user/presentation/UserController.java
@@ -0,0 +1,14 @@
+package org.prgms.locomocoserver.user.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.user.application.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private final UserService userService;
+}
