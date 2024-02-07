999d117210539696901587f3ec31006d0661a1c0
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-07 10:16:56 +0900

    feat: user domain 구성

---
+++ src/main/java/org/prgms/locomocoserver/user/domain/User.java
@@ -0,0 +1,52 @@
+package org.prgms.locomocoserver.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "birth")
+    private LocalDateTime birth;
+
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    @Column(name = "temperature")
+    private int temperature;
+
+    @Column(name = "job", nullable = false)
+    private String job;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "vendor")
+    private String vendor;
+
+    @Builder
+    public User(String nickname, LocalDateTime birth, String gender, int temperature, String job, String email, String vendor) {
+        this.nickname = nickname;
+        this.birth = birth;
+        this.gender = gender;
+        this.temperature = temperature;
+        this.job = job;
+        this.email = email;
+        this.vendor = vendor;
+    }
+}
