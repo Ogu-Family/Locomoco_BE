69e616dbb3a50050ab6fef08a4261d9cdba4232d
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-10 01:01:42 +0900

    refactor: security 필요 없는 설정 삭제

--- src/main/java/org/prgms/locomocoserver/global/config/WebSecurityConfig.java
+++ src/main/java/org/prgms/locomocoserver/global/config/WebSecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -47,10 +45,5 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
