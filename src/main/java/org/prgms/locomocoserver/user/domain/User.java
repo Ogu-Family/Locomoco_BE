8159fab9821ba948aec84eb7919abe5f5765ed84
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-10 00:37:23 +0900

    feat: enum, vo 적용

--- src/main/java/org/prgms/locomocoserver/user/domain/User.java
+++ src/main/java/org/prgms/locomocoserver/user/domain/User.java
@@ -6,8 +6,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.prgms.locomocoserver.global.common.BaseEntity;
+import org.prgms.locomocoserver.user.domain.enums.Gender;
+import org.prgms.locomocoserver.user.domain.enums.Job;
+import org.prgms.locomocoserver.user.domain.enums.Vendor;
 import org.prgms.locomocoserver.user.vo.EmailVo;
+import org.prgms.locomocoserver.user.vo.TemperatureVo;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -23,32 +28,35 @@ public class User extends BaseEntity {
     @Column(name = "nickname", nullable = false)
     private String nickname;
 
-    @Column(name = "birth", nullable = false)
-    private LocalDateTime birth;
+    @Column(name = "birth", nullable = false, columnDefinition = "date")
+    private LocalDate birth;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "gender", nullable = false)
     private String gender;
 
     @Column(name = "temperature", nullable = false)
     private int temperature;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "job", nullable = false)
     private String job;
 
     @Column(name = "email", nullable = false)
     private String email;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "vendor", nullable = false)
     private String vendor;
 
     @Builder
-    public User(String nickname, LocalDateTime birth, String gender, int temperature, String job, String email, String vendor) {
+    public User(String nickname, LocalDate birth, Gender gender, int temperature, Job job, String email, Vendor vendor) {
         this.nickname = nickname;
         this.birth = birth;
-        this.gender = gender;
-        this.temperature = temperature;
-        this.job = job;
+        this.gender = gender.getDisplayName();
+        this.temperature = new TemperatureVo(temperature).getTemperature();
+        this.job = job.getDisplayName();
         this.email = new EmailVo(email).getEmail();
-        this.vendor = vendor;
+        this.vendor = vendor.getDisplayName();
     }
 }
