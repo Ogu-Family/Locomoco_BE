95bb93058c289e14bb8a364a914c9095d8f6eee7
ZZAMBAs <rksidksrksi@naver.com>
2024-02-10 20:37:29 +0900

    chore: User 연관관계 삭제

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/Mogakko.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/Mogakko.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -45,19 +43,14 @@ public class Mogakko {
     @Column(name = "max_participant", columnDefinition = "int default 10")
     private Integer max_participant;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User creator;
-
     @Builder
     public Mogakko(String title, String content, LocalDateTime startTime, LocalDateTime endTime,
-        LocalDateTime deadline, int likeCount, User creator) {
+        LocalDateTime deadline, int likeCount) {
         this.title = title;
         this.content = content;
         this.startTime = startTime;
         this.endTime = endTime;
         this.deadline = deadline;
         this.likeCount = likeCount;
-        this.creator = creator;
     }
 }
