5070440dbab19375b1eb1891a581b0f0dbfe6b11
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-11 17:16:27 +0900

    feat: 첫 로그인일 경우 자동 회원가입 로직 추가

--- src/main/java/org/prgms/locomocoserver/user/application/UserService.java
+++ src/main/java/org/prgms/locomocoserver/user/application/UserService.java
@@ -2,12 +2,27 @@ package org.prgms.locomocoserver.user.application;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.user.domain.User;
 import org.prgms.locomocoserver.user.domain.UserRepository;
+import org.prgms.locomocoserver.user.dto.OAuthUserInfoDto;
+import org.prgms.locomocoserver.user.dto.response.UserDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public UserDto saveOrUpdate(OAuthUserInfoDto userInfoDto) {
+        User user = userRepository.findByEmailAndDeletedAtIsNull(userInfoDto.getEmail())
+                .orElseGet(() -> userRepository.save(userInfoDto.toEntity()));
+
+        // UserDto 생성
+        UserDto userDto = new UserDto(user.getId(), user.getNickname(), user.getBirth().toString(), user.getGender().name(), user.getTemperature(),
+                null, user.getEmail(), user.getProvider(), user.getProvideId());
+        return userDto;
+    }
 }
