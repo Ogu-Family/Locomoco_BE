670380c0a1d495b2bd8a2458a4f488436693ed69
ZZAMBAs <rksidksrksi@naver.com>
2024-02-13 19:27:56 +0900

    feat #18: MogakkoTagRepository 추가

---
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoTagRepository.java
@@ -0,0 +1,7 @@
+package org.prgms.locomocoserver.mogakkos.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MogakkoTagRepository extends JpaRepository<MogakkoTag, Long> {
+
+}
