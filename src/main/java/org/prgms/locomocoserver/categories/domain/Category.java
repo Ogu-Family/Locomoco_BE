87fa5371a7248e693d46cb3f1ce94c1e7173eee8
ZZAMBAs <rksidksrksi@naver.com>
2024-02-13 11:22:10 +0900

    refactor #13: 빌더 수정

--- src/main/java/org/prgms/locomocoserver/categories/domain/Category.java
+++ src/main/java/org/prgms/locomocoserver/categories/domain/Category.java
@@ -12,12 +12,14 @@ import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.prgms.locomocoserver.tags.domain.Tag;
 
 @Entity
 @Getter
+@Builder
 @Table(name = "categories")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
@@ -34,6 +36,7 @@ public class Category {
     private CategoryType categoryType;
 
     @OneToMany(mappedBy = "category")
+    @Builder.Default
     private List<Tag> tags = new ArrayList<>();
 
     public Category(Long id, String name, CategoryType categoryType, List<Tag> tags) {
@@ -41,60 +44,5 @@ public class Category {
         this.name = name;
         this.categoryType = categoryType;
         this.tags = tags;
-    }
-
-    public static CategoryBuilder builder() {
-        return new CategoryBuilder();
-    }
-
-    public static class CategoryBuilder {
-
-        private Long id;
-        private String name;
-        private CategoryType categoryType;
-        private List<Tag> tags;
-        private boolean isTagSet;
-
-        CategoryBuilder() {
-        }
-
-        public CategoryBuilder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public CategoryBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public CategoryBuilder categoryType(CategoryType categoryType) {
-            this.categoryType = categoryType;
-            return this;
-        }
-
-        public CategoryBuilder tags(List<Tag> tags) {
-            this.tags = tags;
-            this.isTagSet = true;
-            return this;
-        }
-
-        public Category build() {
-            return new Category(this.id,
-                this.name,
-                this.categoryType,
-                this.isTagSet ? this.tags : new ArrayList<>());
-        }
-
-        @Override
-        public String toString() {
-            return "CategoryBuilder{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", categoryType=" + categoryType +
-                ", tags=" + tags +
-                ", isTagSet=" + isTagSet +
-                '}';
-        }
     }
 }
