ed3ca5c658b32f765c19099831253553cf60120c
ZZAMBAs <rksidksrksi@naver.com>
2024-02-13 22:45:31 +0900

    feat #18: 모각코 생성 컨트롤러 메서드 추가 및 Swagger 어노테이션 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/dto/request/MogakkoCreateRequestDto.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/dto/request/MogakkoCreateRequestDto.java
@@ -1,19 +1,20 @@
 package org.prgms.locomocoserver.mogakkos.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.prgms.locomocoserver.mogakkos.domain.MGCType;
 import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
 
-public record MogakkoCreateRequestDto(String title,
-                                      String location,
-                                      MGCType mgcType,
-                                      LocalDateTime startTime,
-                                      LocalDateTime endTime,
-                                      LocalDateTime deadline,
-                                      Integer maxParticipants,
-                                      String content,
-                                      List<SelectedTagsDto> tags) {
+public record MogakkoCreateRequestDto(@Schema(description = "모각코 글 제목") String title,
+                                      @Schema(description = "모각코 장소") String location,
+                                      @Schema(description = "모각코 타입") MGCType mgcType,
+                                      @Schema(description = "모각코 시작 시간") LocalDateTime startTime,
+                                      @Schema(description = "모각코 종료 시간") LocalDateTime endTime,
+                                      @Schema(description = "모각코 모집 데드라인 시간") LocalDateTime deadline,
+                                      @Schema(description = "최대 참여자 수") Integer maxParticipants,
+                                      @Schema(description = "모각코 글 내용") String content,
+                                      @Schema(description = "선택된 태그 id 모음") List<SelectedTagsDto> tags) {
 
     public Mogakko toMogakkoWithoutTags() {
         return Mogakko.builder()
--- src/main/java/org/prgms/locomocoserver/mogakkos/presentation/MogakkoController.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/presentation/MogakkoController.java
@@ -1,14 +1,34 @@
 package org.prgms.locomocoserver.mogakkos.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.prgms.locomocoserver.mogakkos.application.MogakkoService;
+import org.prgms.locomocoserver.mogakkos.dto.request.MogakkoCreateRequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Mogakko controller", description = "모각코 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class MogakkoController {
 
     private final MogakkoService mogakkoService;
+
+    @PostMapping("/mogakko/map")
+    @Operation(summary = "모각코 생성", description = "생성에 필요한 값을 받아 모각코를 생성합니다.")
+    @ApiResponses(
+        @ApiResponse(responseCode = "200", description = "모각코 생성 성공")
+    )
+    public ResponseEntity<Long> create(@RequestBody MogakkoCreateRequestDto requestDto) {
+        long mogakkoId = mogakkoService.create(requestDto);
+
+        return ResponseEntity.ok(mogakkoId);
+    }
 }
