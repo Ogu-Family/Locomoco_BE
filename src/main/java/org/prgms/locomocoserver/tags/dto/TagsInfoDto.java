d4190ab9b7e7d225fd4192fa392cba1dcc05c24d
ZZAMBAs <rksidksrksi@naver.com>
2024-02-14 13:21:20 +0900

    chore #20: DTO에 Swagger에 표시할 내용 추가

--- src/main/java/org/prgms/locomocoserver/tags/dto/TagsInfoDto.java
+++ src/main/java/org/prgms/locomocoserver/tags/dto/TagsInfoDto.java
@@ -3,7 +3,7 @@ package org.prgms.locomocoserver.tags.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record TagsInfoDto(@Schema(description = "태그 id") @JsonProperty("tag_id") Long tagId,
-                          @Schema(description = "태그 이름") @JsonProperty("tag_name") String tagName) {
+public record TagsInfoDto(@Schema(description = "태그 id", example = "1") @JsonProperty("tag_id") Long tagId,
+                          @Schema(description = "태그 이름", example = "Typescript") @JsonProperty("tag_name") String tagName) {
 
 }
