2fcfeef8267829e0e90497ad9ebfe878a8a987ed
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-15 07:40:26 +0900

    refactor: 첫 로그인 여부 포함한 로직 변경

--- src/main/java/org/prgms/locomocoserver/user/application/UserService.java
+++ src/main/java/org/prgms/locomocoserver/user/application/UserService.java
@@ -6,9 +6,13 @@ import org.prgms.locomocoserver.user.domain.User;
 import org.prgms.locomocoserver.user.domain.UserRepository;
 import org.prgms.locomocoserver.user.domain.enums.Provider;
 import org.prgms.locomocoserver.user.dto.OAuthUserInfoDto;
-import org.prgms.locomocoserver.user.dto.response.UserDto;
+import org.prgms.locomocoserver.user.dto.response.TokenResponseDto;
+import org.prgms.locomocoserver.user.dto.response.UserInfoDto;
+import org.prgms.locomocoserver.user.dto.response.UserLoginResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,13 +21,21 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public UserDto saveOrUpdate(OAuthUserInfoDto userInfoDto) {
-        User user = userRepository.findByEmailAndDeletedAtIsNull(userInfoDto.getEmail())
-                .orElseGet(() -> userRepository.save(userInfoDto.toEntity()));
+    public UserLoginResponse saveOrUpdate(OAuthUserInfoDto oAuthUserInfoDto, TokenResponseDto tokenResponseDto) {
+        boolean isNewUser = false;
+        Optional<User> optionalUser = userRepository.findByEmailAndDeletedAtIsNull(oAuthUserInfoDto.getEmail());
+
+        User user;
+        if (optionalUser.isPresent()) {
+            user = optionalUser.get();
+        } else {
+            isNewUser = true;
+            user = userRepository.save(oAuthUserInfoDto.toEntity());
+        }
 
         // UserDto 생성
-        UserDto userDto = new UserDto(user.getId(), user.getNickname(), user.getBirth(), user.getGender(), user.getTemperature(),
+        UserInfoDto userDto = new UserInfoDto(user.getId(), user.getNickname(), user.getBirth(), user.getGender(), user.getTemperature(),
                 user.getJob(), user.getEmail(), Provider.valueOf(user.getProvider()));
-        return userDto;
+        return new UserLoginResponse(tokenResponseDto, userDto, isNewUser);
     }
 }
