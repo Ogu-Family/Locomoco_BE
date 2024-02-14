9a1fb3c2b93950f53ce18fcab9a59a515016907f
ZZAMBAs <rksidksrksi@naver.com>
2024-02-14 12:15:38 +0900

    feat #20: 타입에 따른 카테고리와 태그를 전부 반환하는 컨트롤러 추가

--- src/main/java/org/prgms/locomocoserver/categories/dto/response/CategoriesWithTagsDto.java
+++ src/main/java/org/prgms/locomocoserver/categories/dto/response/CategoriesWithTagsDto.java
@@ -1,13 +1,14 @@
 package org.prgms.locomocoserver.categories.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import org.prgms.locomocoserver.global.common.InputType;
 import org.prgms.locomocoserver.tags.dto.TagsInfoDto;
 
-public record CategoriesWithTagsDto(@JsonProperty("category_id") Long categoryId,
-                                    @JsonProperty("category_name") String categoryName,
-                                    @JsonProperty("input_type") InputType inputType,
-                                    List<TagsInfoDto> tags) {
+public record CategoriesWithTagsDto(@Schema(description = "카테고리 id") @JsonProperty("category_id") Long categoryId,
+                                    @Schema(description = "카테고리 이름") @JsonProperty("category_name") String categoryName,
+                                    @Schema(description = "입력받는 폼 타입", example = "DROPDOWN") @JsonProperty("input_type") InputType inputType,
+                                    @Schema(description = "해당 카테고리에 포함된 태그들") List<TagsInfoDto> tags) {
 
 }
--- src/main/java/org/prgms/locomocoserver/tags/dto/TagsInfoDto.java
+++ src/main/java/org/prgms/locomocoserver/tags/dto/TagsInfoDto.java
@@ -1,8 +1,9 @@
 package org.prgms.locomocoserver.tags.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record TagsInfoDto(@JsonProperty("tag_id") Long tagId,
-                          @JsonProperty("tag_name") String tagName) {
+public record TagsInfoDto(@Schema(description = "태그 id") @JsonProperty("tag_id") Long tagId,
+                          @Schema(description = "태그 이름") @JsonProperty("tag_name") String tagName) {
 
 }
