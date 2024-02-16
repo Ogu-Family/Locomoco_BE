26c35231898754c55aa18dbc246a19d245d06a6a
ZZAMBAs <rksidksrksi@naver.com>
2024-02-16 23:56:37 +0900

    feat #33: 모각코 참여자들을 가져오는 쿼리 추가

--- src/main/java/org/prgms/locomocoserver/user/domain/UserRepository.java
+++ src/main/java/org/prgms/locomocoserver/user/domain/UserRepository.java
@@ -1,11 +1,16 @@
 package org.prgms.locomocoserver.user.domain;
 
+import java.util.List;
+import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailAndDeletedAtIsNull(String email);
     Optional<User> findByIdAndDeletedAtIsNull(Long id);
     Optional<User> findByNicknameAndDeletedAtIsNull(String nickname);
+    @Query("SELECT u FROM User u WHERE u.id IN (SELECT p.user.id FROM Participant p WHERE p.mogakko = :mogakko)") // 검증 필
+    List<User> findAllParticipantsByMogakko(Mogakko mogakko);
 }
