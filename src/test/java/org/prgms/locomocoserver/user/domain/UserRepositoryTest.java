25ecef0d736c3d1b3843e4930c8ff76b80242bb2
ZZAMBAs <rksidksrksi@naver.com>
2024-02-17 00:03:49 +0900

    test #33: 모각코 참여자들을 가져오는 쿼리 테스트 추가

---
+++ src/test/java/org/prgms/locomocoserver/user/domain/UserRepositoryTest.java
@@ -0,0 +1,64 @@
+package org.prgms.locomocoserver.user.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
+import org.prgms.locomocoserver.mogakkos.domain.MogakkoRepository;
+import org.prgms.locomocoserver.mogakkos.domain.participants.Participant;
+import org.prgms.locomocoserver.mogakkos.domain.participants.ParticipantRepository;
+import org.prgms.locomocoserver.user.domain.enums.Gender;
+import org.prgms.locomocoserver.user.domain.enums.Job;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ParticipantRepository participantRepository;
+    @Autowired
+    private MogakkoRepository mogakkoRepository;
+
+    @Test
+    @DisplayName("특정 모각코에 참여중인 목록들을 전부 가져올 수 있다.")
+    void success_find_all_participants_by_mogakko() {
+        // given
+        LocalDateTime startTime = LocalDateTime.now();
+
+        User user1 = User.builder().nickname("name").birth(startTime.toLocalDate())
+            .email("email@gmail.com").job(
+                Job.JOB_SEEKER).gender(Gender.MALE).provider("provider").temperature(1).build();
+        User user2 = User.builder().nickname("namae").birth(startTime.toLocalDate())
+            .email("cho@gmail.com").job(
+                Job.JOB_SEEKER).gender(Gender.MALE).provider("provider").temperature(2).build();
+
+        userRepository.saveAll(List.of(user1, user2));
+
+        Mogakko mogakko = Mogakko.builder().title("title").content("content").startTime(
+                startTime).endTime(startTime.plusHours(2)).deadline(startTime.plusHours(1))
+            .likeCount(0).location("어떤 곳").views(0).build();
+
+        Participant participant1 = participantRepository.save(
+            Participant.builder().user(user1).build());
+        Participant participant2 = participantRepository.save(
+            Participant.builder().user(user2).build());
+
+        mogakko.addParticipant(participant1);
+        mogakko.addParticipant(participant2);
+
+        // when
+        Mogakko savedMogakko = mogakkoRepository.save(mogakko);
+
+        // then
+        List<User> allParticipantsByMogakko = userRepository.findAllParticipantsByMogakko(
+            savedMogakko);
+
+        assertThat(allParticipantsByMogakko).hasSize(2);
+    }
+}
