5276d96e03deebedf18d9a1a50ae1142c0a25cac
ZZAMBAs <rksidksrksi@naver.com>
2024-02-17 17:01:11 +0900

    feat #33: Swagger 정보 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/dto/response/MogakkoDetailResponseDto.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/dto/response/MogakkoDetailResponseDto.java
@@ -1,10 +1,11 @@
 package org.prgms.locomocoserver.mogakkos.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import org.prgms.locomocoserver.user.dto.response.UserBriefInfoDto;
 
-public record MogakkoDetailResponseDto(UserBriefInfoDto creatorInfo,
-                                       List<MogakkoParticipantDto> participants,
-                                       MogakkoInfoDto MogakkoInfo) {
+public record MogakkoDetailResponseDto(@Schema(description = "생성자 정보") UserBriefInfoDto creatorInfo,
+                                       @Schema(description = "참여자 목록") List<MogakkoParticipantDto> participants,
+                                       @Schema(description = "모각코 정보") MogakkoInfoDto MogakkoInfo) { // TODO: 문의 추가
 
 }
--- src/main/java/org/prgms/locomocoserver/user/dto/response/UserBriefInfoDto.java
+++ src/main/java/org/prgms/locomocoserver/user/dto/response/UserBriefInfoDto.java
@@ -1,9 +1,10 @@
 package org.prgms.locomocoserver.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.prgms.locomocoserver.user.domain.User;
 
-public record UserBriefInfoDto(Long userId,
-                               String nickname
+public record UserBriefInfoDto(@Schema(description = "유저 id", example = "1") Long userId,
+                               @Schema(description = "유저 닉네임", example = "로코모코") String nickname
                                // TODO: 프로필 사진 처리
                                ) {
 
