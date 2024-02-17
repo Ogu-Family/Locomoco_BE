a565d692265e5356f1d7d54b950962f12523ee4c
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-18 02:21:04 +0900

    feat: 웹소켓 핸들러 기본 틀 구현

---
+++ src/main/java/org/prgms/locomocoserver/chat/WebSocketChatHandler.java
@@ -0,0 +1,89 @@
+package org.prgms.locomocoserver.chat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.prgms.locomocoserver.chat.domain.enums.MessageType;
+import org.prgms.locomocoserver.chat.dto.ChatMessageDto;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketChatHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper mapper;
+    private final Set<WebSocketSession> sessions = new HashSet<>();
+    private final Map<Long, Set<WebSocketSession>> chatRoomSessionMap = new HashMap<>();
+
+    // 소켓 연결 확인
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("{} 연결됨", session.getId());
+        sessions.add(session);
+    }
+
+    // 소켓 통신 시 메세지의 전송을 다루는 부분
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+
+        // 페이로드 -> chatMessageDto로 변환
+        ChatMessageDto chatMessageDto = mapper.readValue(payload, ChatMessageDto.class);
+        log.info("session {}", chatMessageDto.toString());
+
+        Long chatRoomId = chatMessageDto.chatRoomId();
+        // 메모리 상에 채팅방에 대한 세션 없으면 만들어줌
+        if(!chatRoomSessionMap.containsKey(chatRoomId)){
+            chatRoomSessionMap.put(chatRoomId,new HashSet<>());
+        }
+        Set<WebSocketSession> chatRoomSession = chatRoomSessionMap.get(chatRoomId);
+
+        // message 에 담긴 타입을 확인
+        // messageType = ENTER인 경우 session 담기
+        if (chatMessageDto.messageType().equals(MessageType.ENTER)) {
+            chatRoomSession.add(session);
+        }
+        if (chatRoomSession.size()>=3) {
+            removeClosedSession(chatRoomSession);
+        }
+        sendMessageToChatRoom(chatMessageDto, chatRoomSession);
+
+    }
+
+    // 소켓 종료 확인
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("{} 연결 끊김", session.getId());
+        sessions.remove(session);
+    }
+
+    private void removeClosedSession(Set<WebSocketSession> chatRoomSession) {
+        chatRoomSession.removeIf(sess -> !sessions.contains(sess));
+    }
+
+    private void sendMessageToChatRoom(ChatMessageDto chatMessageDto, Set<WebSocketSession> chatRoomSession) {
+        chatRoomSession.parallelStream().forEach(sess -> sendMessage(sess, chatMessageDto));//2
+    }
+
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try{
+            session.sendMessage(new TextMessage(mapper.writeValueAsString(message)));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+}
