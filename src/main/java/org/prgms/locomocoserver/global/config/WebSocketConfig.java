1f9d36d54b5c5574c4e8e51a39be7993c2fc66aa
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-18 15:54:30 +0900

    feat: websocket config stomp 적용

--- src/main/java/org/prgms/locomocoserver/global/config/WebSocketConfig.java
+++ src/main/java/org/prgms/locomocoserver/global/config/WebSocketConfig.java
@@ -2,22 +2,27 @@ package org.prgms.locomocoserver.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
-@RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final WebSocketHandler webSocketHandler;
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // endpoint : /api/v1/chat/{mogakkoId}
-        // ws://localhost:8090/ws/chat 으로 요청이 들어오면 websocket 통신
-        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOrigins("http://localhost:8090")
+                .withSockJS(); // 웹소켓 핸드셰이크 커넥션 생성 경로
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // /pub 으로 시작하는 destination 헤더는 @DestinationMapping 경로 이동
+        config.setApplicationDestinationPrefixes("/pub");
+        //  /sub 으로 시작하는 destination 헤더를 가진 메세지를 브로커로 라우팅
+        config.enableSimpleBroker("/sub");
     }
 }
