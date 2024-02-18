25af8180d52d9c15602e220a6fc46419030ea5f4
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-19 01:57:38 +0900

    feat: chatMessageDto stomp 바탕으로 재구성

--- src/main/java/org/prgms/locomocoserver/chat/dto/ChatMessageDto.java
+++ src/main/java/org/prgms/locomocoserver/chat/dto/ChatMessageDto.java
@@ -1,11 +1,12 @@
 package org.prgms.locomocoserver.chat.dto;
 
-import org.prgms.locomocoserver.chat.domain.enums.MessageType;
-
 public record ChatMessageDto(
-        MessageType messageType,
         Long chatRoomId,
         Long senderId,
         String message
 ) {
+    public ChatMessageDto updateEnterMessage(String newMessage) {
+        return new ChatMessageDto(this.chatRoomId, this.senderId, newMessage);
+    }
 }
+
