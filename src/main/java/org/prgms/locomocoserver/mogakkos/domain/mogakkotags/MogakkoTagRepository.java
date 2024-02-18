1259965a5f8c5236ae988f44e615291e13e727da
ZZAMBAs <rksidksrksi@naver.com>
2024-02-19 00:35:09 +0900

    feat #42: 태그를 받아 모각코-태그 엔티티를 삭제하는 쿼리 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/mogakkotags/MogakkoTagRepository.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/mogakkotags/MogakkoTagRepository.java
@@ -2,6 +2,7 @@ package org.prgms.locomocoserver.mogakkos.domain.mogakkotags;
 
 import java.util.List;
 import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
+import org.prgms.locomocoserver.tags.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +10,5 @@ public interface MogakkoTagRepository extends JpaRepository<MogakkoTag, Long> {
 
     @Query("SELECT mt FROM MogakkoTag mt WHERE mt.mogakko = :mogakko")
     List<MogakkoTag> findAllByMogakko(Mogakko mogakko);
+    void deleteByTag(Tag tag);
 }
