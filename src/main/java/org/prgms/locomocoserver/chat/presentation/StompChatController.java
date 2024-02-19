1f5b75e86493243c9c44ca9870b96f83e8d8b58d
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-20 02:27:17 +0900

    feat: 채팅방 최초 생성 로직, 참여 로직 분리 구현

--- src/main/java/org/prgms/locomocoserver/chat/presentation/StompChatController.java
+++ src/main/java/org/prgms/locomocoserver/chat/presentation/StompChatController.java
@@ -1,22 +1,45 @@
 package org.prgms.locomocoserver.chat.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.chat.application.ChatRoomService;
+import org.prgms.locomocoserver.chat.domain.ChatRoom;
 import org.prgms.locomocoserver.chat.dto.ChatMessageDto;
+import org.prgms.locomocoserver.chat.dto.ChatRoomDto;
+import org.prgms.locomocoserver.chat.dto.request.CreateChatRoomRequest;
+import org.prgms.locomocoserver.user.application.UserService;
+import org.prgms.locomocoserver.user.domain.User;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class StompChatController {
 
     private final SimpMessagingTemplate template;
+    private final ChatRoomService chatRoomService;
+    private final UserService userService;
 
     //Client가 SEND할 수 있는 경로
     //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
     //"/pub/chat/enter"
     @MessageMapping(value = "/chats/enter")
     public void enter(ChatMessageDto message) {
+
+        // 채팅방이 이미 존재하는지 확인합니다.
+        ChatRoom existingRoom = chatRoomService.getById(message.chatRoomId());
+
+        // 채팅방이 존재하지 않으면 새로운 채팅방을 생성
+        if (existingRoom == null) {
+            ChatRoomDto newRoom = chatRoomService.createChatRoom(message);
+            message = new ChatMessageDto(newRoom.getId(), message.senderId(), message.senderId() + "님이 채팅방에 참여하였습니다.");
+        } else {
+            // 이미 존재하는 채팅방에 입장 메시지를 전송
+            message = new ChatMessageDto(existingRoom.getId(), message.senderId(), message.getSenderId() + "님이 채팅방에 참여하였습니다.");
+        }
+
         message = new ChatMessageDto(message.chatRoomId(), message.senderId(), message.senderId() + "님이 채팅방에 참여하였습니다.");
         template.convertAndSend("/sub/chat/room/" + message.chatRoomId(), message);
     }
