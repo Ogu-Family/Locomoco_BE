710e70b4e97b8e010412f635f129cc699ac9e00d
ZZAMBAs <rksidksrksi@naver.com>
2024-02-19 09:49:16 +0900

    feat #42: 수정 서비스 로직 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/application/MogakkoService.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/application/MogakkoService.java
@@ -1,23 +1,28 @@
 package org.prgms.locomocoserver.mogakkos.application;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
 import org.prgms.locomocoserver.mogakkos.domain.MogakkoRepository;
 import org.prgms.locomocoserver.mogakkos.domain.mogakkotags.MogakkoTag;
 import org.prgms.locomocoserver.mogakkos.domain.mogakkotags.MogakkoTagRepository;
 import org.prgms.locomocoserver.mogakkos.dto.request.MogakkoCreateRequestDto;
+import org.prgms.locomocoserver.mogakkos.dto.request.MogakkoUpdateRequestDto;
 import org.prgms.locomocoserver.mogakkos.dto.request.SelectedTagsDto;
 import org.prgms.locomocoserver.mogakkos.dto.response.MogakkoCreateResponseDto;
 import org.prgms.locomocoserver.mogakkos.dto.response.MogakkoDetailResponseDto;
 import org.prgms.locomocoserver.mogakkos.dto.response.MogakkoInfoDto;
 import org.prgms.locomocoserver.mogakkos.dto.response.MogakkoParticipantDto;
+import org.prgms.locomocoserver.mogakkos.dto.response.MogakkoUpdateResponseDto;
 import org.prgms.locomocoserver.tags.domain.Tag;
 import org.prgms.locomocoserver.tags.domain.TagRepository;
 import org.prgms.locomocoserver.user.domain.User;
 import org.prgms.locomocoserver.user.domain.UserRepository;
 import org.prgms.locomocoserver.user.dto.response.UserBriefInfoDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -53,11 +58,66 @@ public class MogakkoService {
         return new MogakkoDetailResponseDto(creatorInfoDto, mogakkoParticipantDtos, mogakkoInfoDto);
     }
 
+    @Transactional
+    public MogakkoUpdateResponseDto update(MogakkoUpdateRequestDto requestDto, Long id) {
+        Mogakko foundMogakko = getByIdNotDeleted(id);
+
+        validateCreator(requestDto, foundMogakko);
+
+        foundMogakko.updateInfo(requestDto.title(), requestDto.content(), requestDto.startTime(),
+            requestDto.endTime(), requestDto.deadline(), requestDto.maxParticipants(),
+            requestDto.location());
+        updateMogakkoTags(foundMogakko, requestDto.tags());
+
+        return new MogakkoUpdateResponseDto(foundMogakko.getId());
+    }
+
+    public Mogakko getByIdNotDeleted(Long id) {
+        return mogakkoRepository.findByIdAndDeletedAtIsNull(id)
+            .orElseThrow(RuntimeException::new); // TODO: 모각코 에러 반환
+    }
+
+    private static void validateCreator(MogakkoUpdateRequestDto requestDto, Mogakko foundMogakko) {
+        if (!foundMogakko.isSameCreatorId(requestDto.updateUserId())) {
+            throw new RuntimeException(); // TODO: 유저 예외 반환
+        }
+    }
+
+    private void updateMogakkoTags(Mogakko updateMogakko, List<Long> updateTagIds) {
+        final int DELETE_TAG = 0;
+        final int MAINTAIN_TAG = 1;
+        final int INSERT_TAG = 2;
+        final Map<Tag, Integer> tagMap = new HashMap<>();
+
+        List<MogakkoTag> existingMogakkoTags = mogakkoTagRepository.findAllByMogakko(updateMogakko);
+        List<Tag> updateTags = tagRepository.findAllById(updateTagIds);
+
+        existingMogakkoTags.forEach(mogakkoTag -> tagMap.put(mogakkoTag.getTag(), DELETE_TAG));
+
+        updateTags.forEach(tag -> {
+            if (tagMap.get(tag) == null) {
+                tagMap.put(tag, INSERT_TAG);
+            }
+            else {
+                tagMap.put(tag, MAINTAIN_TAG);
+            }
+        });
+
+        tagMap.forEach((tag, status) -> {
+            switch (status) {
+                case DELETE_TAG -> mogakkoTagRepository.deleteByTag(tag);
+                case INSERT_TAG -> mogakkoTagRepository.save(
+                    MogakkoTag.builder().mogakko(updateMogakko).tag(tag).build());
+            }
+        });
+    }
+
     private Mogakko createMogakkoBy(MogakkoCreateRequestDto requestDto) {
         List<SelectedTagsDto> selectedTagsDtos = requestDto.tags();
         Mogakko mogakko = requestDto.toMogakkoWithoutTags();
 
-        List<Long> tagIds = selectedTagsDtos.stream().flatMap(dto -> dto.tagIds().stream()).toList();
+        List<Long> tagIds = selectedTagsDtos.stream().flatMap(dto -> dto.tagIds().stream())
+            .toList();
         List<Tag> tags = tagRepository.findAllById(tagIds);
 
         tags.forEach(tag -> {
