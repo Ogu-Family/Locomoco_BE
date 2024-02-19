f61945e1b09ca6a9111236b1fb80870dbba3ac3d
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-20 02:31:28 +0900

    feat: controller명 변경 및 변경된 dto 필드 적용

--- src/main/java/org/prgms/locomocoserver/chat/presentation/ChatController.java
+++ src/main/java/org/prgms/locomocoserver/chat/presentation/ChatController.java
@@ -5,9 +5,7 @@ import org.prgms.locomocoserver.chat.application.ChatRoomService;
 import org.prgms.locomocoserver.chat.domain.ChatRoom;
 import org.prgms.locomocoserver.chat.dto.ChatMessageDto;
 import org.prgms.locomocoserver.chat.dto.ChatRoomDto;
-import org.prgms.locomocoserver.chat.dto.request.CreateChatRoomRequest;
 import org.prgms.locomocoserver.user.application.UserService;
-import org.prgms.locomocoserver.user.domain.User;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class StompChatController {
+public class ChatController {
 
     private final SimpMessagingTemplate template;
     private final ChatRoomService chatRoomService;
-    private final UserService userService;
 
     //Client가 SEND할 수 있는 경로
     //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
@@ -34,13 +31,12 @@ public class StompChatController {
         // 채팅방이 존재하지 않으면 새로운 채팅방을 생성
         if (existingRoom == null) {
             ChatRoomDto newRoom = chatRoomService.createChatRoom(message);
-            message = new ChatMessageDto(newRoom.getId(), message.senderId(), message.senderId() + "님이 채팅방에 참여하였습니다.");
+            message = new ChatMessageDto(newRoom.roomId(), message.mogakkoId(), message.senderId(), message.senderId() + "님이 채팅방에 참여하였습니다.");
         } else {
             // 이미 존재하는 채팅방에 입장 메시지를 전송
-            message = new ChatMessageDto(existingRoom.getId(), message.senderId(), message.getSenderId() + "님이 채팅방에 참여하였습니다.");
+            message = new ChatMessageDto(existingRoom.getId(), message.mogakkoId(), message.senderId(), message.senderId() + "님이 채팅방에 참여하였습니다.");
         }
 
-        message = new ChatMessageDto(message.chatRoomId(), message.senderId(), message.senderId() + "님이 채팅방에 참여하였습니다.");
         template.convertAndSend("/sub/chat/room/" + message.chatRoomId(), message);
     }
 
