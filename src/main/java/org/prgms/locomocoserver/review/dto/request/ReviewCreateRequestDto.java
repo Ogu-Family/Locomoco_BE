e2220e76ff9989915dbcf2a59dd2c95475bc13f7
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-20 11:37:22 +0900

    feat: swagger 정보 추가

--- src/main/java/org/prgms/locomocoserver/review/dto/request/ReviewCreateRequestDto.java
+++ src/main/java/org/prgms/locomocoserver/review/dto/request/ReviewCreateRequestDto.java
@@ -1,10 +1,17 @@
 package org.prgms.locomocoserver.review.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record ReviewCreateRequestDto(
-    Long revieweeId,
-    List<Long> reviewContentId,
-    String content
+        @Schema(description = "리뷰 대상자 id", example = "3")
+        Long revieweeId,
+        @Schema(description = "별점", example = "4")
+        int score,
+        @Schema(description = "리뷰 체크박스 id 리스트", example = "[1, 3, 5]")
+        List<Long> reviewContentId,
+        @Schema(description = "리뷰 내용", example = "와우")
+        String content
 ) {
 }
--- src/main/java/org/prgms/locomocoserver/review/dto/response/ReviewDto.java
+++ src/main/java/org/prgms/locomocoserver/review/dto/response/ReviewDto.java
@@ -1,12 +1,21 @@
 package org.prgms.locomocoserver.review.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record ReviewDto(
+        @Schema(description = "리뷰 id", example = "1")
         Long reviewId,
+        @Schema(description = "리뷰자 id", example = "5")
         Long reviewerId,
+        @Schema(description = "리뷰 대상자 id", example = "2")
         Long revieweeId,
+        @Schema(description = "별점", example = "4")
+        int score,
+        @Schema(description = "체크박스 리뷰 id 리스트", example = "[1, 2, 3]")
         List<Long> reviewContentId,
+        @Schema(description = "리뷰 내용", example = "좋았습니다~")
         String content
 ) {
 }
