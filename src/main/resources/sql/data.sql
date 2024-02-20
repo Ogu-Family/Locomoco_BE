123c7779ac58d8b21ba7e06b2b11b4ab95f79a23
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-20 12:27:33 +0900

    docs: sql 수정

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,13 +1,13 @@
 -- 긍정적인 리뷰 데이터 삽입
-INSERT INTO review_contents (review_content_id, content, is_positive)
+INSERT INTO review_contents (content, is_positive)
 VALUES
-    (1, '친절하고 매너있어요', true),
-    (2, '시간약속을 잘 지켜요', true),
-    (3, '응답이 빨라요', true);
+    ('친절하고 매너있어요', true),
+    ('시간약속을 잘 지켜요', true),
+    ('응답이 빨라요', true);
 
 -- 부정적인 리뷰 데이터 삽입
-INSERT INTO review_contents (review_content_id, content, is_positive)
+INSERT INTO review_contents (content, is_positive)
 VALUES
-    (4, '시간약속을 잘 안지켜요', false),
-    (5, '불친절해요', false),
-    (6, '응답이 느려요', false);
+    ('시간약속을 잘 안지켜요', false),
+    ('불친절해요', false),
+    ('응답이 느려요', false);
