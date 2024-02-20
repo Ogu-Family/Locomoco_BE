296f157e314fcc6d311cacac8c662a5a0f3e84c7
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-20 10:26:16 +0900

    feat: 진행중인 모각코 리스트 조회

--- src/main/java/org/prgms/locomocoserver/user/application/UserService.java
+++ src/main/java/org/prgms/locomocoserver/user/application/UserService.java
@@ -2,6 +2,8 @@ package org.prgms.locomocoserver.user.application;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
+import org.prgms.locomocoserver.mogakkos.dto.response.MogakkoInfoDto;
 import org.prgms.locomocoserver.user.domain.User;
 import org.prgms.locomocoserver.user.domain.UserRepository;
 import org.prgms.locomocoserver.user.domain.enums.Gender;
@@ -14,6 +16,8 @@ import org.prgms.locomocoserver.user.dto.response.UserLoginResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -54,6 +58,15 @@ public class UserService {
     public UserInfoDto getUserInfo(Long userId) {
         User user = getById(userId);
         return UserInfoDto.of(user);
+    }
+
+    public List<MogakkoInfoDto> getOngoingMogakkos(Long userId) {
+        List<Mogakko> mogakkos = userRepository.findOngoingMogakkosByUserId(userId);
+
+        List<MogakkoInfoDto> mogakkoInfoDtos = new ArrayList<>();
+        mogakkos.forEach(mogakko -> mogakkoInfoDtos.add(MogakkoInfoDto.create(mogakko, null)));
+
+        return mogakkoInfoDtos;
     }
 
     public boolean isNicknameUnique(String nickname) {
--- src/main/java/org/prgms/locomocoserver/user/presentation/UserController.java
+++ src/main/java/org/prgms/locomocoserver/user/presentation/UserController.java
@@ -1,6 +1,9 @@
 package org.prgms.locomocoserver.user.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.mogakkos.dto.response.MogakkoInfoDto;
 import org.prgms.locomocoserver.user.application.UserService;
 import org.prgms.locomocoserver.user.dto.request.UserInitInfoRequestDto;
 import org.prgms.locomocoserver.user.dto.response.UserInfoDto;
@@ -8,15 +11,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Tag(name = "User Controller", description = "사용자 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class UserController {
 
     private final UserService userService;
-    private final KakaoController kakaoController;
-    private final GithubController githubController;
 
+    @Operation(summary = "사용자 초기 회원가입", description = "사용자 id로 초기 정보를 입력합니다.")
     @PutMapping("/users/init/{userId}")
     public ResponseEntity<UserInfoDto> getInitInfo(@PathVariable Long userId,
                                                    @RequestBody UserInitInfoRequestDto requestDto) {
@@ -24,14 +29,23 @@ public class UserController {
         return ResponseEntity.ok(userInfoDto);
     }
 
+    @Operation(summary = "회원가입 닉네임 중복확인", description = "닉네임 unique 여부를 확인합니다.")
     @GetMapping("/users/nickname/{nickname}/check")
     public ResponseEntity<Boolean> checkNicknameAvailability(@PathVariable String nickname) {
         return ResponseEntity.ok(userService.isNicknameUnique(nickname));
     }
 
+    @Operation(summary = "마이페이지 정보", description = "사용자 마이페이지 정보를 반환합니다.")
     @GetMapping("/users/{userId}")
     public ResponseEntity<UserInfoDto> getUserInfo(@PathVariable Long userId) {
         UserInfoDto userInfoDto = userService.getUserInfo(userId);
         return ResponseEntity.ok(userInfoDto);
+    }
+
+    @Operation(summary = "진행중인 모각코 목록 조회", description = "사용자가 진행중인 모각코 목록을 조회합니다.")
+    @GetMapping("/users/{userId}/mogakko/ongoing")
+    public ResponseEntity<List<MogakkoInfoDto>> getOngoingMogakkos(@PathVariable Long userId) {
+        List<MogakkoInfoDto> mogakkoInfoDtos = userService.getOngoingMogakkos(userId);
+        return ResponseEntity.ok(mogakkoInfoDtos);
     }
 }
