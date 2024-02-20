c1a4f39448d017a0f20b51b722e974c62b8a8718
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-20 12:12:11 +0900

    feat: review, review content 도메인 구성 및 연결

---
+++ src/main/java/org/prgms/locomocoserver/review/domain/data/ReviewContentRepository.java
@@ -0,0 +1,6 @@
+package org.prgms.locomocoserver.review.domain.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewContentRepository extends JpaRepository<ReviewContent, Long> {
+}
