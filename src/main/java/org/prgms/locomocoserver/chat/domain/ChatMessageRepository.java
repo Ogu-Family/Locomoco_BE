01212b1802c4560e414241fcefb1a4bd4a43cba7
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-21 11:21:22 +0900

    feat: 커서기반 조회 메서드 생성

--- src/main/java/org/prgms/locomocoserver/chat/domain/ChatMessageRepository.java
+++ src/main/java/org/prgms/locomocoserver/chat/domain/ChatMessageRepository.java
@@ -1,6 +1,9 @@
 package org.prgms.locomocoserver.chat.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    Page<ChatMessage> findByChatRoomIdAndDeletedAtIsNullOrderByCreatedAtDesc(Long chatRoomId, Pageable pageable);
 }
