a1a74f8b2214359ce196c9badf313fe87a015459
ZZAMBAs <rksidksrksi@naver.com>
2024-02-21 18:43:40 +0900

    feat #50: 필터링 요청 DTO에서 장소 정보 제거

--- src/main/java/org/prgms/locomocoserver/mogakkos/dto/request/MogakkoFilterRequestDto.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/dto/request/MogakkoFilterRequestDto.java
@@ -3,9 +3,6 @@ package org.prgms.locomocoserver.mogakkos.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
-public record MogakkoFilterRequestDto(@Schema(description = "주소 검색 값", example = "고척동") String address,
-                                      @Schema(description = "지도 상 현재 위도", example = "27.414192012") Double latitude,
-                                      @Schema(description = "지도 상 현재 경도", example = "74.278827613") Double longitude,
-                                      @Schema(description = "필터링 태그 id 목록") List<Long> tags) {
+public record MogakkoFilterRequestDto(@Schema(description = "필터링 태그 id 목록") List<Long> tags) {
 
 }
