938fa33fee4aa49ae46513467e1ee40da92c3a29
ZZAMBAs <rksidksrksi@naver.com>
2024-02-22 22:45:42 +0900

    feat #46: 커서로 모각코들을 가져오는 쿼리 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
@@ -1,8 +1,12 @@
 package org.prgms.locomocoserver.mogakkos.domain;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MogakkoRepository extends JpaRepository<Mogakko, Long> {
     Optional<Mogakko> findByIdAndDeletedAtIsNull(Long id);
+    @Query(value = "SELECT m.* FROM mogakko m JOIN locations l ON l.mogakko_id = m.id WHERE m.id > :cursor LIMIT 20", nativeQuery = true)
+    List<Mogakko> findAll(Long cursor);
 }
