afa70a1dce355238b7fb66004cf22d978a56b091
ZZAMBAs <rksidksrksi@naver.com>
2024-02-22 12:37:05 +0900

    feat #61: 응답 DTO 수정

--- src/main/java/org/prgms/locomocoserver/categories/dto/response/CategoriesWithTagsDto.java
+++ src/main/java/org/prgms/locomocoserver/categories/dto/response/CategoriesWithTagsDto.java
@@ -3,10 +3,12 @@ package org.prgms.locomocoserver.categories.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import org.prgms.locomocoserver.categories.domain.CategoryInputType;
 import org.prgms.locomocoserver.tags.dto.TagsInfoDto;
 
 public record CategoriesWithTagsDto(@Schema(description = "카테고리 id", example = "1") @JsonProperty("category_id") Long categoryId,
                                     @Schema(description = "카테고리 이름", example = "개발 언어") @JsonProperty("category_name") String categoryName,
+                                    @Schema(description = "카테고리 입력 타입", example = "RADIOGROUP") @JsonProperty("input_type") CategoryInputType categoryInputType,
                                     @Schema(description = "해당 카테고리에 포함된 태그들") List<TagsInfoDto> tags) {
 
 }
