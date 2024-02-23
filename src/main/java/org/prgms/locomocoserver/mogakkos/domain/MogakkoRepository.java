dbcd9df721828393aefe18f5f4241590ad2bd6b0
ZZAMBAs <rksidksrksi@naver.com>
2024-02-23 11:02:09 +0900

    feat #63: 전체 검색 쿼리에 city 정보 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface MogakkoRepository extends JpaRepository<Mogakko, Long> {
     Optional<Mogakko> findByIdAndDeletedAtIsNull(Long id);
-    @Query(value = "SELECT m.* FROM mogakko m JOIN locations l ON l.mogakko_id = m.id WHERE m.id > :cursor LIMIT 20", nativeQuery = true)
-    List<Mogakko> findAll(Long cursor);
+    @Query(value = "SELECT m.* FROM mogakko m JOIN locations l ON l.mogakko_id = m.id AND l.city LIKE :city% WHERE m.id > :cursor LIMIT 20", nativeQuery = true)
+    List<Mogakko> findAll(Long cursor, String city);
 }
