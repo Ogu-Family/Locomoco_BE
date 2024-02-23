37a29320203271b8358b0696ffd4535ac526d0b5
ZZAMBAs <rksidksrksi@naver.com>
2024-02-23 22:14:47 +0900

    feat #63: 주소 정보 필터링을 위한 쿼리 수정

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface MogakkoRepository extends JpaRepository<Mogakko, Long> {
     Optional<Mogakko> findByIdAndDeletedAtIsNull(Long id);
-    @Query(value = "SELECT m.* FROM mogakko m JOIN locations l ON (m.id > :cursor AND l.mogakko_id = m.id AND l.city LIKE :city%) LIMIT 20", nativeQuery = true)
+    @Query(value = "SELECT m.* FROM mogakko m "
+        + "JOIN locations l ON (m.id > :cursor AND m.deleted_at IS NULL AND l.mogakko_id = m.id AND l.city LIKE :city%) "
+        + "LIMIT 20", nativeQuery = true)
     List<Mogakko> findAll(Long cursor, String city);
 }
