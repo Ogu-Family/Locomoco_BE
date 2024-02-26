d94338809bc905c113a148b2899c6dcf93e33f4b
eugene <81062608+eugene225@users.noreply.github.com>
2024-02-26 11:24:01 +0900

    feat: 사용자 받은 리뷰 조회 기능 구현

--- src/main/java/org/prgms/locomocoserver/review/application/ReviewService.java
+++ src/main/java/org/prgms/locomocoserver/review/application/ReviewService.java
@@ -1,14 +1,28 @@
 package org.prgms.locomocoserver.review.application;
 
 import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.review.domain.Review;
 import org.prgms.locomocoserver.review.domain.ReviewRepository;
+import org.prgms.locomocoserver.review.dto.response.ReviewDto;
+import org.prgms.locomocoserver.user.application.UserService;
+import org.prgms.locomocoserver.user.domain.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final UserService userService;
 
+    @Transactional(readOnly = true)
+    public List<ReviewDto> getRecievedReviews(Long userId) {
+        User user = userService.getById(userId);
 
+        return reviewRepository.findAllByRevieweeAndDeletedAtIsNull(user).stream()
+                .map(review -> ReviewDto.of(review)).toList();
+    }
 }
