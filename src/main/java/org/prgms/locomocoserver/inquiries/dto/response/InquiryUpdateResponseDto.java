3927347cfe230b1fc2553caf0abb09bd11cbc342
ZZAMBAs <rksidksrksi@naver.com>
2024-02-26 21:35:34 +0900

    feat #65: 정적 팩토리 메서드 추가

--- src/main/java/org/prgms/locomocoserver/inquiries/dto/response/InquiryUpdateResponseDto.java
+++ src/main/java/org/prgms/locomocoserver/inquiries/dto/response/InquiryUpdateResponseDto.java
@@ -1,7 +1,11 @@
 package org.prgms.locomocoserver.inquiries.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.prgms.locomocoserver.inquiries.domain.Inquiry;
 
 public record InquiryUpdateResponseDto(@Schema(description = "수정된 문의 id", example = "312") Long id) {
 
+    public static InquiryUpdateResponseDto create(Inquiry inquiry) {
+        return new InquiryUpdateResponseDto(inquiry.getId());
+    }
 }
