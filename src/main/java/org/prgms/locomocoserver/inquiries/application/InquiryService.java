42b4bbcc7fbffb20e91e1d5ca8422cf4f197cacc
ZZAMBAs <rksidksrksi@naver.com>
2024-03-03 22:41:29 +0900

    feat #66: 문의 삭제 서비스 메서드 구현

--- src/main/java/org/prgms/locomocoserver/inquiries/application/InquiryService.java
+++ src/main/java/org/prgms/locomocoserver/inquiries/application/InquiryService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.prgms.locomocoserver.inquiries.domain.Inquiry;
 import org.prgms.locomocoserver.inquiries.domain.InquiryRepository;
 import org.prgms.locomocoserver.inquiries.dto.request.InquiryCreateRequestDto;
+import org.prgms.locomocoserver.inquiries.dto.request.InquiryDeleteRequestDto;
 import org.prgms.locomocoserver.inquiries.dto.request.InquiryUpdateRequestDto;
 import org.prgms.locomocoserver.inquiries.dto.response.InquiryUpdateResponseDto;
 import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
@@ -11,6 +12,7 @@ import org.prgms.locomocoserver.mogakkos.domain.MogakkoRepository;
 import org.prgms.locomocoserver.user.domain.User;
 import org.prgms.locomocoserver.user.domain.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -35,7 +37,7 @@ public class InquiryService {
         Inquiry foundInquiry = inquiryRepository.findByIdAndDeletedAtIsNull(id)
             .orElseThrow(RuntimeException::new);// TODO: 문의 예외 반환
 
-        validateUser(requestDto, foundInquiry);
+        validateUser(requestDto.userId(), foundInquiry);
 
         foundInquiry.updateInfo(requestDto.content());
         Inquiry updatedInquiry = inquiryRepository.save(foundInquiry);
@@ -43,8 +45,18 @@ public class InquiryService {
         return InquiryUpdateResponseDto.create(updatedInquiry);
     }
 
-    private static void validateUser(InquiryUpdateRequestDto requestDto, Inquiry foundInquiry) {
-        boolean isSameUser = foundInquiry.getUser().getId().equals(requestDto.userId());
+    @Transactional
+    public void delete(Long id, InquiryDeleteRequestDto requestDto) {
+        Inquiry inquiry = inquiryRepository.findByIdAndDeletedAtIsNull(id)
+            .orElseThrow(RuntimeException::new); // TODO: 문의 예외 반환
+
+        validateUser(requestDto.userId(), inquiry);
+
+        inquiry.delete();
+    }
+
+    private static void validateUser(Long inquiryUserId, Inquiry foundInquiry) {
+        boolean isSameUser = foundInquiry.getUser().getId().equals(inquiryUserId);
 
         if (!isSameUser) {
             throw new RuntimeException(); // TODO: 유저 예외 반환
