551be18e1a2ff5a886295415c71504a73cb81918
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-04 01:35:39 +0900

    feat: s3 이미지 저장 기능 구현

--- src/main/java/org/prgms/locomocoserver/image/application/ImageService.java
+++ src/main/java/org/prgms/locomocoserver/image/application/ImageService.java
@@ -1,13 +1,96 @@
 package org.prgms.locomocoserver.image.application;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.image.domain.Image;
 import org.prgms.locomocoserver.image.domain.ImageRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
 
     private final ImageRepository imageRepository;
+    private final AmazonS3 amazonS3;
 
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    private final String dirName = "upload/";
+
+    public Image upload(MultipartFile multipartFile) throws IOException {
+        File file = convertMultipartFileToFile(multipartFile);
+
+        return upload(file, dirName + LocalDate.now().getYear());
+    }
+
+    private Image upload(File file, String dirName) {
+        String key = randomFileName(file, dirName);
+        String path = putS3(file, key);
+        removeFile(file);
+
+        return imageRepository.save(Image
+                .builder()
+                .key(key)
+                .path(path)
+                .build());
+    }
+
+    private String randomFileName(File file, String dirName) {
+        return dirName + "/" + file.getName();
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        return getS3(bucket, fileName);
+    }
+
+    private String getS3(String bucket, String fileName) {
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeFile(File file) {
+        file.delete();
+    }
+
+    public File convertMultipartFileToFile(MultipartFile multipartFile) throws IOException {
+        // 확장자 추출
+        String originalFilename = multipartFile.getOriginalFilename();
+        String extension = "";
+        int lastDotIndex = originalFilename.lastIndexOf(".");
+        if (lastDotIndex != -1) {
+            extension = originalFilename.substring(lastDotIndex);
+        }
+
+        // 임시 파일 생성
+        String randomFilename = UUID.randomUUID().toString(); // 확장자를 포함하지 않은 임시 파일 이름
+        File file = File.createTempFile(randomFilename, extension);
+
+        // MultipartFile의 데이터를 생성한 임시 파일에 복사
+        try (FileOutputStream fos = new FileOutputStream(file)){
+            fos.write(multipartFile.getBytes());
+        }
+
+        // 임시 파일 반환
+        return file;
+    }
+
+    public void remove(Image image) {
+        if (!amazonS3.doesObjectExist(bucket, image.getKey())) {
+            throw new AmazonS3Exception("Object " +image.getKey()+ " does not exist!");
+        }
+        amazonS3.deleteObject(bucket, image.getKey());
+    }
 }
--- src/main/java/org/prgms/locomocoserver/image/presentaion/ImageController.java
+++ src/main/java/org/prgms/locomocoserver/image/presentaion/ImageController.java
@@ -2,8 +2,11 @@ package org.prgms.locomocoserver.image.presentaion;
 
 import lombok.RequiredArgsConstructor;
 import org.prgms.locomocoserver.image.application.ImageService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.prgms.locomocoserver.image.domain.Image;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -11,4 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImageController {
 
     private final ImageService imageService;
+
+    @PostMapping("/images")
+    public Image upload(@RequestPart("file") MultipartFile multipartFile) throws IOException {
+        return imageService.upload(multipartFile);
+    }
+
+    @DeleteMapping("/images")
+    public void remove(Image image) {
+        imageService.remove(image);
+    }
 }
