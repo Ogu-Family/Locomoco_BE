82a1eb60b2919637f3bf901f1f8951c969d6ac92
ZZAMBAs <rksidksrksi@naver.com>
2024-03-04 23:24:23 +0900

    feat #66: 문의 가져오는 쿼리문 각각 생성

--- src/main/java/org/prgms/locomocoserver/inquiries/domain/InquiryRepository.java
+++ src/main/java/org/prgms/locomocoserver/inquiries/domain/InquiryRepository.java
@@ -1,8 +1,30 @@
 package org.prgms.locomocoserver.inquiries.domain;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
     Optional<Inquiry> findByIdAndDeletedAtIsNull(Long id);
+    @Query(value = "SELECT i.* FROM inquiries i WHERE id < :cursor "
+        + "ORDER BY i.created_at DESC "
+        + "LIMIT 20", nativeQuery = true)
+    List<Inquiry> findAll(Long cursor);
+    @Query(value = "SELECT i.* FROM inquiries i "
+        + "INNER JOIN users u ON i.id < :cursor AND u.id = :userId AND i.user_id = u.id "
+        + "ORDER BY i.created_at DESC "
+        + "LIMIT 20", nativeQuery = true)
+    List<Inquiry> findAllByUser(Long cursor, Long userId);
+    @Query(value = "SELECT i.* FROM inquiries i "
+        + "INNER JOIN mogakko m ON i.id < :cursor AND m.id = :mogakkoId AND i.mogakko_id = m.id "
+        + "ORDER BY i.created_at DESC "
+        + "LIMIT 20", nativeQuery = true)
+    List<Inquiry> findAllByMogakko(Long cursor, Long mogakkoId);
+    @Query(value = "SELECT i.* FROM inquiries i "
+        + "INNER JOIN mogakko m ON i.id < :cursor AND m.id = :mogakkoId AND i.mogakko_id = m.id "
+        + "INNER JOIN users u ON u.id = :userId AND i.user_id = u.id "
+        + "ORDER BY i.created_at DESC "
+        + "LIMIT 20", nativeQuery = true)
+    List<Inquiry> findAllByMogakkoAndUser(Long cursor, Long mogakkoId, Long userId);
 }
