e20a56685b37097501b92e4a411f05c1d044f108
ZZAMBAs <rksidksrksi@naver.com>
2024-03-04 16:00:56 +0900

    feat #82: 통합 검색 쿼리 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
@@ -15,6 +15,20 @@ public interface MogakkoRepository extends JpaRepository<Mogakko, Long> {
         + "JOIN locations l ON (m.id > :cursor AND m.deleted_at IS NULL AND l.mogakko_id = m.id AND l.city LIKE :city%) "
         + "LIMIT 20", nativeQuery = true)
     List<Mogakko> findAllByCity(Long cursor, String city);
+    @Query(value = "SELECT DISTINCT m.* FROM mogakko m "
+        + "JOIN users u ON m.id > :cursor AND m.deleted_at IS NULL AND m.creator_id = u.id "
+        + "JOIN locations l "
+        + "JOIN mogakko_tags mt ON mt.tag_id IN :tagIds "
+        + "WHERE (m.title LIKE %:searchVal% OR m.content LIKE %:searchVal% OR u.nickname LIKE %:searchVal% OR l.city LIKE %searchVal%) "
+        + "GROUP BY m.id HAVING count(m.id) = :tagSize "
+        + "LIMIT 20", nativeQuery = true)
+    List<Mogakko> findAllByFilter(Long cursor, String searchVal, List<Long> tagIds, int tagSize);
+    @Query(value = "SELECT DISTINCT m.* FROM mogakko m "
+        + "JOIN users u ON m.id > :cursor AND m.deleted_at IS NULL AND m.creator_id = u.id "
+        + "JOIN locations l "
+        + "WHERE (m.title LIKE %:searchVal% OR m.content LIKE %:searchVal% OR u.nickname LIKE %:searchVal% OR l.city LIKE %searchVal%) "
+        + "LIMIT 20", nativeQuery = true)
+    List<Mogakko> findAllByFilter(Long cursor, String searchVal);
     @Query("SELECT m FROM Mogakko m JOIN m.participants p WHERE p.user = :user AND m.endTime >= :now AND m.deletedAt IS NULL AND p.user.deletedAt IS NULL")
     List<Mogakko> findOngoingMogakkosByUser(@Param("user") User user, @Param("now") LocalDateTime now);
     @Query("SELECT m FROM Mogakko m JOIN m.participants p WHERE p.user = :user AND m.endTime < :now AND m.deletedAt IS NULL AND p.user.deletedAt IS NULL")
