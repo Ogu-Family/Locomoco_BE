b9f0b1bb4ff6d8a5348d2ebd2016323c77055d43
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-05 02:30:27 +0900

    refactor: 최초 입장 시에만 입장 메시지 전달되도록 수정

--- src/main/java/org/prgms/locomocoserver/chat/presentation/StompChatController.java
+++ src/main/java/org/prgms/locomocoserver/chat/presentation/StompChatController.java
@@ -3,14 +3,17 @@ package org.prgms.locomocoserver.chat.presentation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.prgms.locomocoserver.chat.application.ChatRoomService;
+import org.prgms.locomocoserver.chat.domain.ChatMessageRepository;
+import org.prgms.locomocoserver.chat.domain.ChatRoom;
+import org.prgms.locomocoserver.chat.domain.ChatRoomRepository;
 import org.prgms.locomocoserver.chat.dto.ChatMessageDto;
-import org.prgms.locomocoserver.chat.dto.ChatRoomDto;
 import org.prgms.locomocoserver.chat.dto.request.ChatMessageRequestDto;
 import org.prgms.locomocoserver.user.application.UserService;
-import org.prgms.locomocoserver.user.domain.User;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -19,16 +22,20 @@ public class StompChatController {
 
     private final SimpMessagingTemplate template;
     private final ChatRoomService chatRoomService;
-    private final UserService userService;
+    private final ChatRoomRepository chatRoomRepository;
 
     @MessageMapping(value = "/chats/enter")
     public void enter(ChatMessageRequestDto requestDto) {
+        Optional<ChatRoom> chatRoom = chatRoomRepository.findByIdAndDeletedAtIsNull(requestDto.chatRoomId());
 
-        ChatRoomDto chatRoomDto = chatRoomService.enterChatRoom(requestDto);
-        User sender = userService.getById(requestDto.senderId());
-        requestDto = new ChatMessageRequestDto(chatRoomDto.roomId(), requestDto.senderId(), requestDto.mogakkoId(), sender.getNickname() + "님이 채팅방에 참여하였습니다.");
+        ChatMessageDto chatMessageDto;
+        if (chatRoom != null && !chatRoomService.isParticipantExist(chatRoom.get(), requestDto.senderId())) {
+            chatMessageDto = chatRoomService.saveEnterMessage(requestDto);
+            chatRoomService.addParticipant(chatRoom.get(), requestDto.senderId());
+            template.convertAndSend("/sub/chat/room/" + requestDto.chatRoomId(), chatMessageDto);
+        }
 
-        template.convertAndSend("/sub/chat/room/" + requestDto.chatRoomId(), requestDto);
+        chatRoomService.enterChatRoom(requestDto);
     }
 
     @MessageMapping(value = "/chats/message")
