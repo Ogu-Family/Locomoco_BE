0c8af550fba456725e137d1142737b40b52d2e45
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-05 00:55:40 +0900

    기존 user dto에 프로필 이미지 필드 추가

--- src/main/java/org/prgms/locomocoserver/user/dto/response/UserBriefInfoDto.java
+++ src/main/java/org/prgms/locomocoserver/user/dto/response/UserBriefInfoDto.java
@@ -1,14 +1,15 @@
 package org.prgms.locomocoserver.user.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.prgms.locomocoserver.image.dto.ImageDto;
 import org.prgms.locomocoserver.user.domain.User;
 
 public record UserBriefInfoDto(@Schema(description = "유저 id", example = "1") Long userId,
-                               @Schema(description = "유저 닉네임", example = "로코모코") String nickname
-                               // TODO: 프로필 사진 처리
+                               @Schema(description = "유저 닉네임", example = "로코모코") String nickname,
+                               @Schema(description = "유저 프로필 이미지", example = "") ImageDto profileImage
                                ) {
 
     public static UserBriefInfoDto of(User user) {
-        return new UserBriefInfoDto(user.getId(), user.getNickname());
+        return new UserBriefInfoDto(user.getId(), user.getNickname(), ImageDto.of(user.getProfileImage()));
     }
 }
