13978fceed8708b3dbab972b79d3aa4a9de59377
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-05 22:32:59 +0900

    feat: LikeRepository 생성

---
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/likes/LikeRepository.java
@@ -0,0 +1,6 @@
+package org.prgms.locomocoserver.mogakkos.domain.likes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
