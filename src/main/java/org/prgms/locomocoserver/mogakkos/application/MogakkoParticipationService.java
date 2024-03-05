ad7b164520014c344272e54da283c67d93fb91b1
ZZAMBAs <rksidksrksi@naver.com>
2024-03-05 21:46:19 +0900

    feat #87: 참여 로직 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/application/MogakkoParticipationService.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/application/MogakkoParticipationService.java
@@ -2,9 +2,13 @@ package org.prgms.locomocoserver.mogakkos.application;
 
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
 import org.prgms.locomocoserver.mogakkos.domain.participants.Participant;
 import org.prgms.locomocoserver.mogakkos.domain.participants.ParticipantRepository;
+import org.prgms.locomocoserver.mogakkos.dto.request.ParticipationRequestDto;
 import org.prgms.locomocoserver.mogakkos.dto.response.ParticipationCheckingDto;
+import org.prgms.locomocoserver.user.application.UserService;
+import org.prgms.locomocoserver.user.domain.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +16,8 @@ import org.springframework.stereotype.Service;
 public class MogakkoParticipationService {
 
     private final ParticipantRepository participantRepository;
-
+    private final MogakkoService mogakkoService;
+    private final UserService userService;
 
     public ParticipationCheckingDto check(Long mogakkoId, Long userId) {
         Optional<Participant> optionalParticipant = participantRepository.findByMogakkoIdAndUserId(
@@ -23,5 +28,14 @@ public class MogakkoParticipationService {
         }
 
         return new ParticipationCheckingDto(false);
+    }
+
+    public void participate(Long mogakkoId, ParticipationRequestDto requestDto) {
+        Mogakko mogakko = mogakkoService.getByIdNotDeleted(mogakkoId);
+        User user = userService.getById(requestDto.userId());
+
+        Participant participant = Participant.builder().mogakko(mogakko).user(user).build();
+        mogakko.addParticipant(participant);
+        participantRepository.save(participant);
     }
 }
--- src/main/java/org/prgms/locomocoserver/mogakkos/presentation/MogakkoParticipationController.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/presentation/MogakkoParticipationController.java
@@ -3,10 +3,13 @@ package org.prgms.locomocoserver.mogakkos.presentation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.prgms.locomocoserver.mogakkos.application.MogakkoParticipationService;
+import org.prgms.locomocoserver.mogakkos.dto.request.ParticipationRequestDto;
 import org.prgms.locomocoserver.mogakkos.dto.response.ParticipationCheckingDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,13 @@ public class MogakkoParticipationController {
         ParticipationCheckingDto responseDto = participationService.check(id, userId);
 
         return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("mogakko/map/{id}/participate")
+    public ResponseEntity<Void> participate(@PathVariable Long id,
+        @RequestBody ParticipationRequestDto requestDto) {
+        participationService.participate(id, requestDto);
+
+        return ResponseEntity.ok().build();
     }
 }
