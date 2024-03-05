8133522f941f6ae98751c5e26e91c0866caea6eb
ZZAMBAs <rksidksrksi@naver.com>
2024-03-05 21:20:39 +0900

    feat #87: 참여 중인지 확인하는 컨트롤러 메서드 추가

---
+++ src/main/java/org/prgms/locomocoserver/mogakkos/presentation/MogakkoParticipationController.java
@@ -0,0 +1,29 @@
+package org.prgms.locomocoserver.mogakkos.presentation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.mogakkos.application.MogakkoParticipationService;
+import org.prgms.locomocoserver.mogakkos.dto.response.ParticipationCheckingDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Mogakko Participation controller", description = "모각코 참여 컨트롤러")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class MogakkoParticipationController {
+
+    private final MogakkoParticipationService participationService;
+
+    @GetMapping("mogakko/map/{id}/participate")
+    public ResponseEntity<ParticipationCheckingDto> checkParticipating(
+        @PathVariable Long id, @RequestParam Long userId) {
+        ParticipationCheckingDto responseDto = participationService.check(id, userId);
+
+        return ResponseEntity.ok(responseDto);
+    }
+}
