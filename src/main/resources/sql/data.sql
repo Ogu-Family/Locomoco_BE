e83a7f8637691a7cbf72fcc033f5221a61d97893
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-06 01:55:30 +0900

    docs: 더미데이터 추가

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -12,12 +12,26 @@ VALUES
     ('불친절해요', false),
     ('응답이 느려요', false);
 
--- 모각코 더미 데이터
-INSERT INTO mogakko (created_at, deleted_at, updated_at, content, deadline, end_time, like_count, location, max_participants, start_time, title, views, creator_id, inquiry_id, user_id)
+-- 더미 데이터 삽입
+INSERT INTO users (created_at, email, gender, job, nickname, provider, temperature)
 VALUES
-    (NOW(), NULL, NOW(), '더미 내용 1', '2024-02-21 12:00:00', '2024-02-21 14:00:00', 5, '더미 위치 1', 10, '2024-02-21 10:00:00', '더미 제목 1', 100, 1, NULL, 1),
-    (NOW(), NULL, NOW(), '더미 내용 2', '2024-02-22 12:00:00', '2024-02-22 14:00:00', 8, '더미 위치 2', 15, '2024-02-22 10:00:00', '더미 제목 2', 120, 2, NULL, 2),
-    (NOW(), NULL, NOW(), '더미 내용 3', '2024-02-23 12:00:00', '2024-02-23 14:00:00', 10, '더미 위치 3', 20, '2024-02-23 10:00:00', '더미 제목 3', 150, 3, NULL, 3);
+    (NOW(), 'user1@example.com', 'MALE', 'DEVELOPER', 'User1', 'GITHUB', 36.5),
+    (NOW(), 'user2@example.com', 'FEMALE', 'JOB_SEEKER', 'User2', 'KAKAO', 36.6),
+    (NOW(), 'user3@example.com', 'MALE', 'ETC', 'User3', 'GITHUB', 36.7);
+
+-- 모각코 더미 데이터
+INSERT INTO mogakko (created_at, deleted_at, updated_at, content, deadline, end_time, like_count, max_participants, start_time, title, views, creator_id, inquiry_id, user_id)
+VALUES
+    (NOW(), NULL, NOW(), '더미 내용 1', '2024-02-21 12:00:00', '2024-02-21 14:00:00', 5, 10, '2024-02-21 10:00:00', '더미 제목 1', 100, 1, NULL, 1),
+    (NOW(), NULL, NOW(), '더미 내용 2', '2024-02-22 12:00:00', '2024-02-22 14:00:00', 8, 15, '2024-02-22 10:00:00', '더미 제목 2', 120, 2, NULL, 1),
+    (NOW(), NULL, NOW(), '더미 내용 3', '2024-02-23 12:00:00', '2024-02-23 14:00:00', 10, 20, '2024-02-23 10:00:00', '더미 제목 3', 150, 3, NULL, 2);
+
+-- 모각코 위치 더미 데이터
+INSERT INTO locations (created_at, latitude, longitude, mogakko_id)
+VALUES
+    (NOW(), 37.1234567, 127.1234567, 4),
+    (NOW(), 37.2345678, 127.2345678, 5),
+    (NOW(), 37.3456789, 127.3456789, 6);
 
 
 -- 카테고리 데이터
