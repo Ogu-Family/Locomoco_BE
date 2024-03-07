62b9a5e5fe3f7c4d675cf373df0a99e2a17f5d54
ZZAMBAs <rksidksrksi@naver.com>
2024-03-07 23:02:07 +0900

    feat #103: 블랙리스트를 가져오는 서비스 메서드 생성

---
+++ src/main/java/org/prgms/locomocoserver/blacklist/application/BlacklistService.java
@@ -0,0 +1,27 @@
+package org.prgms.locomocoserver.blacklist.application;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.blacklist.domain.Blacklist;
+import org.prgms.locomocoserver.blacklist.domain.BlacklistRepository;
+import org.prgms.locomocoserver.global.common.dto.Results;
+import org.prgms.locomocoserver.user.dto.response.UserBriefInfoDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BlacklistService {
+
+    private final BlacklistRepository blacklistRepository;
+
+    @Transactional(readOnly = true)
+    public Results<UserBriefInfoDto> findAll(Long cursor, Long userId) {
+        List<Blacklist> blacklist = blacklistRepository.findAllByBlockUserId(cursor, userId);
+
+        List<UserBriefInfoDto> blackedUsers = blacklist.stream()
+            .map(black -> UserBriefInfoDto.of(black.getBlockedUser())).toList();
+
+        return new Results<>(blackedUsers);
+    }
+}
