75ccd4ef264423b6615cc416cd130daccfeeace4
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-08 01:11:51 +0900

    fix: update request dto 컬럼 수정

--- src/main/java/org/prgms/locomocoserver/report/dto/request/ReportUpdateRequest.java
+++ src/main/java/org/prgms/locomocoserver/report/dto/request/ReportUpdateRequest.java
@@ -1,11 +1,8 @@
 package org.prgms.locomocoserver.report.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.NonNull;
 
 public record ReportUpdateRequest(
-        @NonNull
-        Long reportId,
         @NotBlank
         String content
 ) {
