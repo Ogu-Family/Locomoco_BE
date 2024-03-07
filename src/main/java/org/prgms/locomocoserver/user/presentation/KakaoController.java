5a3fc32cea82bdecb6fca6ee8e24e86dfef21c34
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-07 20:32:16 +0900

    docs: 로그인관련 중요정보 로그 삭제

--- src/main/java/org/prgms/locomocoserver/user/presentation/KakaoController.java
+++ src/main/java/org/prgms/locomocoserver/user/presentation/KakaoController.java
@@ -49,16 +49,13 @@ public class KakaoController {
         log.info("KakaoController.getKakaoLoginCallback " + code);
 
         TokenResponseDto tokenResponseDto = getTokenDto(code);
-
-        log.info("token: " + tokenResponseDto.accessToken());
-
         KakaoUserInfoResponseDto kakaoUserInfoResponseDto = loadUserInfo(tokenResponseDto.accessToken());
 
         log.info("KakaoController.getKakaoLoginCallback after loadUserInfo : " + kakaoUserInfoResponseDto.getEmail());
 
         UserLoginResponse userLoginResponse = userService.saveOrUpdate(kakaoUserInfoResponseDto, tokenResponseDto);
 
-        log.info("KakaoController.getKakaoLoginCallback after saveOrUpdate : " + userLoginResponse.tokenResponseDto().accessToken());
+        log.info("KakaoController.getKakaoLoginCallback after saveOrUpdate : " + userLoginResponse.isNewUser());
 
         return ResponseEntity.ok(userLoginResponse);
     }
@@ -88,8 +85,6 @@ public class KakaoController {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(tokenUrl, HttpMethod.POST, new HttpEntity<>(requestBody, headers), String.class);
 
-        log.info("KakaoController.getTokenDto response : " + response.getBody());
-
         // 응답 본문에서 DTO로 수동 변환
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(response.getBody(), TokenResponseDto.class);
@@ -99,8 +94,6 @@ public class KakaoController {
         // Kakao API 엔드포인트 URL
         String apiUrl = "https://kapi.kakao.com/v2/user/me";
 
-        log.info("KakaoController.loadUserInfo start : " + accessToken);
-
         // HTTP 요청 헤더 설정
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + accessToken);
@@ -108,8 +101,6 @@ public class KakaoController {
         // REST 템플릿을 사용하여 GET 요청 전송
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, new HttpEntity<>(headers), String.class);
-
-        log.info("KakaoController.loadUserInfo response : " + response.getBody());
 
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(response.getBody(), KakaoUserInfoResponseDto.class);
