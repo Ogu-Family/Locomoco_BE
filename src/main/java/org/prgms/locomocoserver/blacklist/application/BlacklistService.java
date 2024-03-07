c3edff80761fce8d83c4add36902feff1cdb6296
ZZAMBAs <rksidksrksi@naver.com>
2024-03-07 23:54:58 +0900

    feat #103: 블랙리스트 등록 로직 추가

--- src/main/java/org/prgms/locomocoserver/blacklist/application/BlacklistService.java
+++ src/main/java/org/prgms/locomocoserver/blacklist/application/BlacklistService.java
@@ -4,7 +4,10 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.prgms.locomocoserver.blacklist.domain.Blacklist;
 import org.prgms.locomocoserver.blacklist.domain.BlacklistRepository;
+import org.prgms.locomocoserver.blacklist.dto.request.BlacklistRequestDto;
 import org.prgms.locomocoserver.global.common.dto.Results;
+import org.prgms.locomocoserver.user.application.UserService;
+import org.prgms.locomocoserver.user.domain.User;
 import org.prgms.locomocoserver.user.dto.response.UserBriefInfoDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class BlacklistService {
 
     private final BlacklistRepository blacklistRepository;
+    private final UserService userService;
 
     @Transactional(readOnly = true)
     public Results<UserBriefInfoDto> findAll(Long cursor, Long userId) {
@@ -23,5 +27,21 @@ public class BlacklistService {
             .map(black -> UserBriefInfoDto.of(black.getBlockedUser())).toList();
 
         return new Results<>(blockedUsers);
+    }
+
+    public void black(Long userId, BlacklistRequestDto requestDto) {
+        User blockUser = userService.getById(userId);
+        User blockedUser = userService.getById(requestDto.blockedId());
+
+        validateUser(blockUser, blockedUser);
+
+        Blacklist blacklist = Blacklist.builder().blockUser(blockUser).blockedUser(blockedUser).build();
+        blacklistRepository.save(blacklist);
+    }
+
+    private void validateUser(User user1, User user2) { // TODO: 이 메서드 추후 따로 클래스로 빼기 (InquiryService에 동일한 메서드 존재)
+        if (user1 == user2) {
+            throw new RuntimeException("자기 자신은 블랙리스트에 넣을 수 없습니다!"); // TODO: 블랙리스트 예외 반환
+        }
     }
 }
