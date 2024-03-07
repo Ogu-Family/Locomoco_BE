12913c4a5e0a1a1c188a09af85b248a3d934ae97
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-08 01:29:00 +0900

    feat: report 전체조회 커서기반 조회 쿼리 작성

--- src/main/java/org/prgms/locomocoserver/report/domain/ReportRepository.java
+++ src/main/java/org/prgms/locomocoserver/report/domain/ReportRepository.java
@@ -1,11 +1,13 @@
 package org.prgms.locomocoserver.report.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
     Optional<Report> findByIdAndDeletedAtIsNull(Long id);
-    List<Report> findAllByDeletedAtIsNull();
+    @Query("SELECT r FROM Report r WHERE r.deletedAt IS NULL AND r.id > :cursor ORDER BY r.id")
+    List<Report> findAllByDeletedAtIsNull(Long cursor, int pageSize);
 }
