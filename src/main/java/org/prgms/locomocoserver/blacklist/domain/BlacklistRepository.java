d30aec67864c5b075ca1110a5db59e837ee5b0a4
ZZAMBAs <rksidksrksi@naver.com>
2024-03-07 23:07:38 +0900

    feat #103: 블랙리스트를 가져오는 컨트롤러 메서드 생성

--- src/main/java/org/prgms/locomocoserver/blacklist/domain/BlacklistRepository.java
+++ src/main/java/org/prgms/locomocoserver/blacklist/domain/BlacklistRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface BlacklistRepository extends JpaRepository<Blacklist, Long> {
-    @Query(value = "SELECT * FROM blacklist "
+    @Query(value = "SELECT * FROM blacklists "
         + "WHERE id > :cursor AND blocker_id = :userId "
         + "ORDER BY blocked_id LIMIT 20",
         nativeQuery = true)
