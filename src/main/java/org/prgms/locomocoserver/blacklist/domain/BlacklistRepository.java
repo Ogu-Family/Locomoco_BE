ad526c615a9d4c419e6162c665b5257ff12831a3
ZZAMBAs <rksidksrksi@naver.com>
2024-03-07 22:59:53 +0900

    feat #103: 블랙리스트를 가져오는 쿼리 생성

---
+++ src/main/java/org/prgms/locomocoserver/blacklist/domain/BlacklistRepository.java
@@ -0,0 +1,13 @@
+package org.prgms.locomocoserver.blacklist.domain;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BlacklistRepository extends JpaRepository<Blacklist, Long> {
+    @Query(value = "SELECT * FROM blacklist "
+        + "WHERE id > :cursor AND blocker_id = :userId "
+        + "ORDER BY blocked_id LIMIT 20",
+        nativeQuery = true)
+    List<Blacklist> findAllByBlockUserId(Long cursor, Long userId);
+}
