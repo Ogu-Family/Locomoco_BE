6ac6b2dfe7d1a6a7f57c32c1ab8ab8d9482efa27
ZZAMBAs <rksidksrksi@naver.com>
2024-03-08 00:18:31 +0900

    feat #103: 블랙리스트 삭제 서비스 로직 추가

--- src/main/java/org/prgms/locomocoserver/blacklist/application/BlacklistService.java
+++ src/main/java/org/prgms/locomocoserver/blacklist/application/BlacklistService.java
@@ -8,6 +8,7 @@ import org.prgms.locomocoserver.blacklist.dto.request.BlacklistRequestDto;
 import org.prgms.locomocoserver.global.common.dto.Results;
 import org.prgms.locomocoserver.user.application.UserService;
 import org.prgms.locomocoserver.user.domain.User;
+import org.prgms.locomocoserver.user.domain.UserRepository;
 import org.prgms.locomocoserver.user.dto.response.UserBriefInfoDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class BlacklistService {
 
     private final BlacklistRepository blacklistRepository;
+    private final UserRepository userRepository;
     private final UserService userService;
 
     @Transactional(readOnly = true)
@@ -37,6 +39,16 @@ public class BlacklistService {
 
         Blacklist blacklist = Blacklist.builder().blockUser(blockUser).blockedUser(blockedUser).build();
         blacklistRepository.save(blacklist);
+    }
+
+    public void delete(Long userId, Long blockedId) {
+        User blockUser = userRepository.getReferenceById(userId);
+        User blockedUser = userRepository.getReferenceById(blockedId);
+
+        Blacklist blacklist = blacklistRepository.findByBlockUserAndBlockedUser(blockUser, blockedUser)
+            .orElseThrow(() -> new RuntimeException("블랙리스트로 등록되어 있지 않습니다."));
+
+        blacklistRepository.delete(blacklist);
     }
 
     private void validateUser(User user1, User user2) { // TODO: 이 메서드 추후 따로 클래스로 빼기 (InquiryService에 동일한 메서드 존재)
