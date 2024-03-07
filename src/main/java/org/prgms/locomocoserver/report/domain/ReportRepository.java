6e536584c41f49665bb000f003380c5add283709
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-08 01:12:31 +0900

    feat: repository id 기반 조회 메서드 생성

--- src/main/java/org/prgms/locomocoserver/report/domain/ReportRepository.java
+++ src/main/java/org/prgms/locomocoserver/report/domain/ReportRepository.java
@@ -2,5 +2,8 @@ package org.prgms.locomocoserver.report.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    Optional<Report> findByIdAndDeletedAtIsNull(Long id);
 }
