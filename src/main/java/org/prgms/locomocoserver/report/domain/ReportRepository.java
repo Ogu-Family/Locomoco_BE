f998e5177a68a74555f0f7014627d129479e2bf3
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-08 09:19:10 +0900

    refactor: 쿼리문 pageSize 활용 수정

--- src/main/java/org/prgms/locomocoserver/report/domain/ReportRepository.java
+++ src/main/java/org/prgms/locomocoserver/report/domain/ReportRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
     Optional<Report> findByIdAndDeletedAtIsNull(Long id);
-    @Query("SELECT r FROM Report r WHERE r.deletedAt IS NULL AND r.id > :cursor ORDER BY r.id")
+    @Query(value = "SELECT * FROM  reports r WHERE r.deleted_at IS NULL AND r.id > :cursor ORDER BY r.id limit :pageSize", nativeQuery = true)
     List<Report> findAllByDeletedAtIsNull(Long cursor, int pageSize);
 }
