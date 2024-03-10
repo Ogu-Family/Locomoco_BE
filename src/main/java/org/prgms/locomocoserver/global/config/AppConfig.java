53557dfed336280984b4e75c44958e426385829d
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-10 23:02:21 +0900

    feat: cors filter 생성 및 빈등록

--- src/main/java/org/prgms/locomocoserver/global/config/AppConfig.java
+++ src/main/java/org/prgms/locomocoserver/global/config/AppConfig.java
@@ -1,6 +1,7 @@
 package org.prgms.locomocoserver.global.config;
 
 import org.prgms.locomocoserver.global.filter.AuthenticationFilter;
+import org.prgms.locomocoserver.global.filter.CorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,14 @@ public class AppConfig {
         FilterRegistrationBean<AuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(filter);
         registrationBean.addUrlPatterns("/api/v1/chats/rooms/*"); // 필터를 적용할 URL 패턴 지정
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration(CorsFilter filter) {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns("/api/v1/*"); // 적절한 URL 패턴으로 변경해야 함
         return registrationBean;
     }
 
