4012273e277753432c4e3031c34fe657aa377101
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-10 23:02:54 +0900

    fix: 기존 cors 처리 빈 삭제

--- src/main/java/org/prgms/locomocoserver/global/config/WebConfig.java
+++ src/main/java/org/prgms/locomocoserver/global/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
