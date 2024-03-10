879e5a86be040aac8050004a44e658e8940930bd
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-10 17:23:20 +0900

    refactor: chat 조회 관련 page 반환 쿼리문 수정 후 list 반환하도록 변경

--- src/main/java/org/prgms/locomocoserver/chat/domain/ChatRoomRepository.java
+++ src/main/java/org/prgms/locomocoserver/chat/domain/ChatRoomRepository.java
@@ -1,17 +1,15 @@
 package org.prgms.locomocoserver.chat.domain;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByIdAndDeletedAtIsNull(Long id);
 
-    @Query(value = "SELECT * FROM chat_rooms cr JOIN users_chat_room_list ucr ON cr.id = ucr.chat_room_list_id WHERE ucr.user_id = :userId AND cr.id > :cursorId ORDER BY cr.id DESC",
-            countQuery = "SELECT COUNT(*) FROM chat_rooms cr JOIN users_chat_room_list ucr ON cr.id = ucr.chat_room_list_id WHERE ucr.user_id = :userId",
+    @Query(value = "SELECT * FROM chat_rooms cr JOIN users_chat_room_list ucr ON cr.id = ucr.chat_room_list_id WHERE ucr.user_id = :userId AND cr.id > :cursorId ORDER BY cr.id DESC limit :pageSize",
             nativeQuery = true)
-    Page<ChatRoom> findByParticipantsId(Long userId, Long cursorId, Pageable pageable);
+    List<ChatRoom> findByParticipantsId(Long userId, Long cursorId, int pageSize);
 }
