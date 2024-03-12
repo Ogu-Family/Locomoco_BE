b924d60401bcb8732a5e52d49c10cd2596a445f6
ZZAMBAs <rksidksrksi@naver.com>
2024-03-12 13:27:35 +0900

    feat #124: 채팅방 생성 DTO 추가 및 기존 ChatMessageRequestDto 수정

--- src/main/java/org/prgms/locomocoserver/chat/dto/request/ChatMessageRequestDto.java
+++ src/main/java/org/prgms/locomocoserver/chat/dto/request/ChatMessageRequestDto.java
@@ -3,11 +3,7 @@ package org.prgms.locomocoserver.chat.dto.request;
 import lombok.NonNull;
 import org.prgms.locomocoserver.chat.domain.ChatMessage;
 import org.prgms.locomocoserver.chat.domain.ChatRoom;
-import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
 import org.prgms.locomocoserver.user.domain.User;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public record ChatMessageRequestDto(
         @NonNull
@@ -18,20 +14,11 @@ public record ChatMessageRequestDto(
         Long senderId,
         String message // TODO: 글자수 초과 시 예외 처리
 ) {
-
-    public ChatRoom toChatRoomEntity(Mogakko mogakko, User creator) {
-        return ChatRoom.builder()
-                .name(mogakko.getTitle())
-                .mogakko(mogakko)
-                .creator(creator)
-                .build();
-    }
-
     public ChatMessage toChatMessageEntity(User sender, ChatRoom chatRoom) {
         return ChatMessage.builder()
-                .sender(sender)
-                .chatRoom(chatRoom)
-                .content(message).build();
+            .sender(sender)
+            .chatRoom(chatRoom)
+            .content(message).build();
     }
 
     public ChatMessage toEnterMessageEntity(User sender, ChatRoom chatRoom) {
