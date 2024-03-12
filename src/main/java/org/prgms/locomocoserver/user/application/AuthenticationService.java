1f4eb9112852bf815ed65caf80742a464fa86586
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-13 03:40:15 +0900

    feat: filter token 관련 커스텀 예외 적용

--- src/main/java/org/prgms/locomocoserver/user/application/AuthenticationService.java
+++ src/main/java/org/prgms/locomocoserver/user/application/AuthenticationService.java
@@ -1,8 +1,12 @@
 package org.prgms.locomocoserver.user.application;
 
 import lombok.RequiredArgsConstructor;
+import org.prgms.locomocoserver.global.exception.ExpiredTokenException;
+import org.prgms.locomocoserver.global.exception.InvalidTokenException;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -12,15 +16,23 @@ public class AuthenticationService {
     public boolean authenticateKakaoUser(String accessToken) {
         String url = "https://kapi.kakao.com/v1/user/access_token_info";
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", accessToken); // header로 넘어오는 accessToken은 Bearer 붙어있음
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", accessToken); // header로 넘어오는 accessToken은 Bearer 붙어있음
 
-        HttpEntity<String> entity = new HttpEntity<>(headers);
+            HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
-        return response.getStatusCode() == HttpStatus.OK;
+            return response.getStatusCode() == HttpStatus.OK;
+        } catch (HttpClientErrorException.Unauthorized e) {
+            throw new ExpiredTokenException("Kakao access token expired");
+        } catch (HttpClientErrorException.BadRequest e) {
+            throw new InvalidTokenException("Invalid Kakao access token");
+        } catch (HttpServerErrorException e) {
+            throw new RuntimeException("Error occurred during Kakao authentication", e);
+        }
     }
 
     public boolean authenticateGithubUser(String accessToken) {
