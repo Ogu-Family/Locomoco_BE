e98ba284018310082ff34afd0403754b7510bfb8
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-13 23:31:33 +0900

    feat: filter에서 발생하는 exception 처리 filter 추가

---
+++ src/main/java/org/prgms/locomocoserver/global/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,60 @@
+package org.prgms.locomocoserver.global.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import org.prgms.locomocoserver.global.exception.ErrorCode;
+import org.prgms.locomocoserver.global.exception.ExpiredTokenException;
+import org.prgms.locomocoserver.global.exception.InvalidTokenException;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Order(3)
+@Component
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        try{
+            filterChain.doFilter(request, response);
+        }catch (ExpiredTokenException e){
+            //토큰의 유효기간 만료
+            setErrorResponse(response, ErrorCode.ACCESSTOKEN_EXPIRED);
+        }catch (InvalidTokenException | IllegalArgumentException e){
+            //유효하지 않은 토큰
+            setErrorResponse(response, ErrorCode.INVALID_TOKEN);
+        }
+    }
+    private void setErrorResponse(
+            HttpServletResponse response,
+            ErrorCode errorCode
+    ){
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+        try{
+            response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Data
+    public static class ErrorResponse{
+        private final Integer code;
+        private final String message;
+    }
+}
