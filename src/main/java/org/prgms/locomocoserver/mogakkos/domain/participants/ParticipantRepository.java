8fad9c62580507bcfbd7ce568e6a5095224ebd18
ZZAMBAs <rksidksrksi@naver.com>
2024-03-14 10:28:18 +0900

    feat #130: 개수 세는 count 쿼리 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/participants/ParticipantRepository.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/participants/ParticipantRepository.java
@@ -1,8 +1,19 @@
 package org.prgms.locomocoserver.mogakkos.domain.participants;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import org.prgms.locomocoserver.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
     Optional<Participant> findByMogakkoIdAndUserId(Long mogakkoId, Long userId);
+
+    @Query("SELECT COUNT(p.id) FROM Participant p INNER JOIN Mogakko m "
+        + "ON p.user = :user AND m.endTime >= :time AND p.mogakko = m AND m.deletedAt IS NULL")
+    long countOngoingByUser(User user, LocalDateTime time);
+
+    @Query("SELECT COUNT(p.id) FROM Participant p INNER JOIN Mogakko m "
+        + "ON p.user = :user AND m.endTime < :time AND p.mogakko = m AND m.deletedAt IS NULL")
+    long countCompleteByUser(User user, LocalDateTime time);
 }
