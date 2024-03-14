1085a699dca74c069dfb72b298234e0397f4911c
ZZAMBAs <rksidksrksi@naver.com>
2024-03-14 13:28:40 +0900

    feat #131: DTO에 생성 시간, 수정 시간 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/dto/response/MogakkoSimpleInfoResponseDto.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/dto/response/MogakkoSimpleInfoResponseDto.java
@@ -1,6 +1,7 @@
 package org.prgms.locomocoserver.mogakkos.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.prgms.locomocoserver.location.domain.Location;
 import org.prgms.locomocoserver.location.dto.LocationInfoDto;
@@ -10,6 +11,8 @@ public record MogakkoSimpleInfoResponseDto(@Schema(description = "모각코 id")
                                            @Schema(description = "제목") String title,
                                            @Schema(description = "조회 수") long views,
                                            @Schema(description = "좋아요(찜) 수") int likeCount,
+                                           @Schema(description = "생성 시간") LocalDateTime createdAt,
+                                           @Schema(description = "수정 시간") LocalDateTime updatedAt,
                                            @Schema(description = "최대 참여자 수") int maxParticipants,
                                            @Schema(description = "현재 참여자 수") int curParticipants,
                                            @Schema(description = "장소 정보") LocationInfoDto location,
@@ -20,6 +23,8 @@ public record MogakkoSimpleInfoResponseDto(@Schema(description = "모각코 id")
             mogakko.getTitle(),
             mogakko.getViews(),
             mogakko.getLikeCount(),
+            mogakko.getCreatedAt(),
+            mogakko.getUpdatedAt(),
             mogakko.getMaxParticipants(),
             mogakko.getParticipants().size(),
             LocationInfoDto.create(location),
