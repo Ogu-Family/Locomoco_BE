12dd0ea67c8d40e07bddca600fb31108af8ad9e6
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-14 20:09:02 +0900

    feat: 채팅 메시지 길이 validation 추가

--- src/main/java/org/prgms/locomocoserver/chat/dto/request/ChatMessageRequestDto.java
+++ src/main/java/org/prgms/locomocoserver/chat/dto/request/ChatMessageRequestDto.java
@@ -3,6 +3,7 @@ package org.prgms.locomocoserver.chat.dto.request;
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.NotBlank;
 
+import org.hibernate.validator.constraints.Length;
 import org.prgms.locomocoserver.chat.domain.ChatMessage;
 import org.prgms.locomocoserver.chat.domain.ChatRoom;
 import org.prgms.locomocoserver.user.domain.User;
@@ -14,8 +15,8 @@ public record ChatMessageRequestDto(
         Long mogakkoId,
         @Nonnull
         Long senderId,
-        @NotBlank
-        String message // TODO: 글자수 초과 시 예외 처리
+        @NotBlank @Length(max = 255, message = "글자수는 255자까지 입력 가능합니다.")
+        String message
 ) {
     public ChatMessage toChatMessageEntity(User sender, ChatRoom chatRoom, boolean isNotice) {
         return ChatMessage.builder()
