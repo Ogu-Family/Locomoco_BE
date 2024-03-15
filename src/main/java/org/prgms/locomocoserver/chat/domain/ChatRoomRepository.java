85f166f4ddca5bba0f4520f14f5c43a169b4562a
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-15 10:45:45 +0900

    fix: 변경된 테이블 쿼리 적용

--- src/main/java/org/prgms/locomocoserver/chat/domain/ChatRoomRepository.java
+++ src/main/java/org/prgms/locomocoserver/chat/domain/ChatRoomRepository.java
@@ -9,8 +9,9 @@ import java.util.Optional;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByIdAndDeletedAtIsNull(Long id);
 
-    @Query(value = "SELECT * FROM chat_rooms cr JOIN users_chat_room_list ucr ON cr.id = ucr.chat_room_list_id WHERE ucr.user_id = :userId AND cr.id < :cursorId ORDER BY cr.id DESC limit :pageSize",
+    @Query(value = "SELECT * FROM chat_rooms cr JOIN chat_participants cp ON cr.id = cp.user_id WHERE cp.user_id = :userId AND cr.id < :cursorId ORDER BY cr.id DESC limit :pageSize",
             nativeQuery = true)
     List<ChatRoom> findByParticipantsId(Long userId, Long cursorId, int pageSize);
+
     Optional<ChatRoom> findByMogakkoId(Long mogakkoId);
 }
