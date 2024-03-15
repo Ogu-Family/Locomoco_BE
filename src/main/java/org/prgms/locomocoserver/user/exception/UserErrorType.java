5bdf69330b00493e9e4c6342b60639ac96f00f27
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-16 01:45:54 +0900

    feat: user 형식 관련 에러타입 추가

--- src/main/java/org/prgms/locomocoserver/user/exception/UserErrorType.java
+++ src/main/java/org/prgms/locomocoserver/user/exception/UserErrorType.java
@@ -6,7 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserErrorType {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자가 존재하지 않습니다."),
-    PROVIDER_TYPE_ERROR(HttpStatus.UNAUTHORIZED, "해당 provider가 존재하지 않습니다.")
+    PROVIDER_TYPE_ERROR(HttpStatus.UNAUTHORIZED, "해당 provider가 존재하지 않습니다."),
+    BIRTH_TYPE_ERROR(HttpStatus.BAD_REQUEST, "생년월일이 형식에 맞지 않습니다."),
+    EMAIL_TYPE_ERROR(HttpStatus.BAD_REQUEST, "이메일이 형식에 맞지 않습니다."),
+    TEMPERATURE_TYPE_ERROR(HttpStatus.BAD_REQUEST, "온도는 0도 이상 100도 이하의 값이어야 합니다.")
     ;
 
     private final HttpStatus status;
