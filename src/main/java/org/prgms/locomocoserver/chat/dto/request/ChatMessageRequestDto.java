8d5639c61a11daf2ff5a70aaf1fd027a857de982
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-15 09:14:53 +0900

    fix: 사용하지 않는 필드 삭제

--- src/main/java/org/prgms/locomocoserver/chat/dto/request/ChatMessageRequestDto.java
+++ src/main/java/org/prgms/locomocoserver/chat/dto/request/ChatMessageRequestDto.java
@@ -2,7 +2,6 @@ package org.prgms.locomocoserver.chat.dto.request;
 
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.NotBlank;
-
 import org.hibernate.validator.constraints.Length;
 import org.prgms.locomocoserver.chat.domain.ChatMessage;
 import org.prgms.locomocoserver.chat.domain.ChatRoom;
@@ -11,8 +10,6 @@ import org.prgms.locomocoserver.user.domain.User;
 public record ChatMessageRequestDto(
         @Nonnull
         Long chatRoomId,
-        @Nonnull
-        Long mogakkoId,
         @Nonnull
         Long senderId,
         @NotBlank @Length(max = 255, message = "글자수는 255자까지 입력 가능합니다.")
