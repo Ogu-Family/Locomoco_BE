ff898f49a636e7be842e0e15a0c64626b4f735fd
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-18 11:35:02 +0900

    fix: cors 추가

--- src/main/java/org/prgms/locomocoserver/global/filter/CorsFilter.java
+++ src/main/java/org/prgms/locomocoserver/global/filter/CorsFilter.java
@@ -21,7 +21,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000, http://locomoco.kro.kr");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
