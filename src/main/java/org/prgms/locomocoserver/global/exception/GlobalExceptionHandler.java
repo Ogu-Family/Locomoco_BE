af3324c8c703d0e682798c8575dfd0ef5619b957
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-18 10:27:04 +0900

    refactor: exception handler 통합

---
+++ src/main/java/org/prgms/locomocoserver/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.prgms.locomocoserver.global.exception;
+
+import org.prgms.locomocoserver.image.exception.ImageException;
+import org.prgms.locomocoserver.user.exception.UserException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<Object> handleUserException(UserException ex) {
+        return ResponseEntity.status(ex.getErrorType().getStatus()).body(ex.getErrorType().getMessage());
+    }
+
+    @ExceptionHandler(ImageException.class)
+    public ResponseEntity<Object> handleImageException(ImageException ex) {
+        return ResponseEntity.status(ex.getErrorType().getStatus()).body(ex.getErrorType().getMessage());
+    }
+}
