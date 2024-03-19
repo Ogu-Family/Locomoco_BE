8a51c851f9305392e9ec46be52ed181b0fc075f5
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-19 18:45:13 +0900

    feat: 채팅방 참여자 삭제 메서드 추가

--- src/main/java/org/prgms/locomocoserver/chat/domain/ChatParticipantRepository.java
+++ src/main/java/org/prgms/locomocoserver/chat/domain/ChatParticipantRepository.java
@@ -3,5 +3,5 @@ package org.prgms.locomocoserver.chat.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
-
+    void deleteAllByChatRoom(ChatRoom chatRoom);
 }
