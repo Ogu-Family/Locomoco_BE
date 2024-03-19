2e52f64659bde0b69b07456fad3e411c1060b951
ZZAMBAs <rksidksrksi@naver.com>
2024-03-19 10:02:17 +0900

    refactor #160: MogakkoTagRepository 의존 관계 제거

--- src/main/java/org/prgms/locomocoserver/mogakkos/dto/SearchRepositoryDto.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/dto/SearchRepositoryDto.java
@@ -1,9 +1,6 @@
 package org.prgms.locomocoserver.mogakkos.dto;
 
 import org.prgms.locomocoserver.mogakkos.domain.MogakkoRepository;
-import org.prgms.locomocoserver.mogakkos.domain.mogakkotags.MogakkoTagRepository;
 
-public record SearchRepositoryDto (MogakkoRepository mogakkoRepository,
-                                   MogakkoTagRepository mogakkoTagRepository
-                                   ) {
+public record SearchRepositoryDto (MogakkoRepository mogakkoRepository) {
 }
