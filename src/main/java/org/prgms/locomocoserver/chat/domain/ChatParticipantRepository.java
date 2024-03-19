61e1fa3d1e0295e282612ad5f46c3c7b4d635cbc
eugene <81062608+eugene225@users.noreply.github.com>
2024-03-19 19:02:26 +0900

    Feat #168 : 사용자 id기반 채팅방 나가기 쿼리 메서드 추가

--- src/main/java/org/prgms/locomocoserver/chat/domain/ChatParticipantRepository.java
+++ src/main/java/org/prgms/locomocoserver/chat/domain/ChatParticipantRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
     void deleteAllByChatRoom(ChatRoom chatRoom);
+    void deleteByChatRoomIdAndUserId(Long chatRoomId, Long userId);
 }
