a26fa7e316f368c29ab8efe40cf3e84dc09cdb65
eugene <81062608+eugene225@users.noreply.github.com>
2024-04-01 01:45:25 +0900

    feat #173 : redis refresh token 도메인 구성

---
+++ src/main/java/org/prgms/locomocoserver/user/domain/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package org.prgms.locomocoserver.user.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByAccessToken(String accessToken);
+}
