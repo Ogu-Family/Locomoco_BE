816ca33496194b67512dbbaa04f06c1efaeb8ed6
eugene <81062608+eugene225@users.noreply.github.com>
2024-04-03 02:40:46 +0900

    feat #173 : delete 쿼리문 추가

--- src/main/java/org/prgms/locomocoserver/user/domain/RefreshTokenRepository.java
+++ src/main/java/org/prgms/locomocoserver/user/domain/RefreshTokenRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
     Optional<RefreshToken> findByAccessToken(String accessToken);
+    void deleteByAccessToken(String refreshToken);
 }
