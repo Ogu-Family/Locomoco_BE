11ce227f2959f76119d7a19344b75f0fb4771482
eugene <81062608+eugene225@users.noreply.github.com>
2024-04-04 01:40:42 +0900

    feat #173 : 사용자 토큰 관련 에러타입 추가

--- src/main/java/org/prgms/locomocoserver/user/exception/UserErrorType.java
+++ src/main/java/org/prgms/locomocoserver/user/exception/UserErrorType.java
@@ -9,7 +9,9 @@ public enum UserErrorType {
     PROVIDER_TYPE_ERROR(HttpStatus.UNAUTHORIZED, "해당 provider가 존재하지 않습니다."),
     BIRTH_TYPE_ERROR(HttpStatus.BAD_REQUEST, "생년월일이 형식에 맞지 않습니다."),
     EMAIL_TYPE_ERROR(HttpStatus.BAD_REQUEST, "이메일이 형식에 맞지 않습니다."),
-    TEMPERATURE_TYPE_ERROR(HttpStatus.BAD_REQUEST, "온도는 0도 이상 100도 이하의 값이어야 합니다.")
+    TEMPERATURE_TYPE_ERROR(HttpStatus.BAD_REQUEST, "온도는 0도 이상 100도 이하의 값이어야 합니다."),
+    ACCESSTOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 엑세스 토큰이 존재하지 않습니다."),
+    REFRESHTOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "리프레시 토큰이 만료되었습니다.")
     ;
 
     private final HttpStatus status;
