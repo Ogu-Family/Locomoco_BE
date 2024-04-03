5b0cccb4d37c4634da3c3831961e0dd67fac5eaf
ZZAMBAs <rksidksrksi@naver.com>
2024-04-03 23:43:16 +0900

    test #173: Redis 테스트 컨테이너 추가

--- src/test/java/org/prgms/locomocoserver/user/application/RefreshTokenServiceTest.java
+++ src/test/java/org/prgms/locomocoserver/user/application/RefreshTokenServiceTest.java
@@ -1,18 +1,19 @@
 package org.prgms.locomocoserver.user.application;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.prgms.locomocoserver.global.AbstractRedisContainerBaseTest;
 import org.prgms.locomocoserver.user.domain.RefreshTokenRepository;
 import org.prgms.locomocoserver.user.dto.response.TokenResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
-class RefreshTokenServiceTest {
+class RefreshTokenServiceTest extends AbstractRedisContainerBaseTest {
 
     @Autowired
     private RefreshTokenService refreshTokenService;
