e5abae7edc4407521c9bd57377975d073c11b7e6
ZZAMBAs <rksidksrksi@naver.com>
2024-04-04 14:43:45 +0900

    fix #173: yml 값을 동적으로 변경하는 코드로 수정

--- src/test/java/org/prgms/locomocoserver/global/AbstractRedisContainerBaseTest.java
+++ src/test/java/org/prgms/locomocoserver/global/AbstractRedisContainerBaseTest.java
@@ -1,11 +1,17 @@
 package org.prgms.locomocoserver.global;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class AbstractRedisContainerBaseTest {
+    static final Logger log = LoggerFactory.getLogger(AbstractRedisContainerBaseTest.class);
 
     static final String REDIS_IMAGE = "redis:7-alpine";
     static final int REDIS_PORT = 6379;
@@ -15,10 +21,17 @@ public abstract class AbstractRedisContainerBaseTest {
     static {
         REDIS_CONTAINER = new GenericContainer<>(REDIS_IMAGE)
             .withExposedPorts(REDIS_PORT)
+            .waitingFor(Wait.forListeningPort())
             .withReuse(true);
         REDIS_CONTAINER.start();
+    }
 
-        System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
-        System.setProperty("spring.data.redis.port", String.valueOf(REDIS_CONTAINER.getMappedPort(REDIS_PORT)));
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry){
+        log.info("spring.data.redis.host: {}, spring.data.redis.port: {}", REDIS_CONTAINER.getHost(),
+            REDIS_CONTAINER.getMappedPort(REDIS_PORT));
+
+        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", () -> String.valueOf(REDIS_CONTAINER.getMappedPort(REDIS_PORT)));
     }
 }
