33317c09f31b716c1042692bec697230263375f4
ZZAMBAs <rksidksrksi@naver.com>
2024-04-06 02:31:36 +0900

    feat: 프로파일 설정 추가

--- src/test/java/org/prgms/locomocoserver/user/application/TestRedisConfig.java
+++ src/test/java/org/prgms/locomocoserver/user/application/TestRedisConfig.java
@@ -1,40 +1,33 @@
-package org.prgms.locomocoserver.global.config;
+package org.prgms.locomocoserver.user.application;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
+@TestConfiguration
 @EnableRedisRepositories
-@Profile("prod")
-public class RedisConfig {
-
+@Profile("test")
+public class TestRedisConfig {
     @Value("${spring.data.redis.host}")
     private String redisHost;
 
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    @Value("${spring.data.redis.password}")
-    private String redisPassword;
-
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(redisHost);
         redisStandaloneConfiguration.setPort(redisPort);
-        redisStandaloneConfiguration.setPassword(redisPassword);
 
-        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder().useSsl().build();
-        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfiguration);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
