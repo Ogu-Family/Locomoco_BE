d901c9d83efc7ee56a26edb051fad066df168e3b
eugene <81062608+eugene225@users.noreply.github.com>
2024-04-06 00:52:55 +0900

    fix: redis연결 ssl 통신 적용

--- src/main/java/org/prgms/locomocoserver/global/config/RedisConfig.java
+++ src/main/java/org/prgms/locomocoserver/global/config/RedisConfig.java
@@ -3,8 +3,12 @@ package org.prgms.locomocoserver.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -24,21 +28,25 @@ public class RedisConfig {
     private String redisPassword;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
+    public LettuceConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(redisHost);
-        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPort(redisPort); // Redis 포트 번호
         redisStandaloneConfiguration.setPassword(redisPassword);
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+        redisStandaloneConfiguration.setDatabase(0);
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl() // SSL 사용 설정
+                .build();
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
-        // StringRedisSerializer 직렬화 방법 설정
-        redisTemplate.setKeySerializer(new StringRedisSerializer());    // 문자열 key 저장
-        redisTemplate.setValueSerializer(new StringRedisSerializer());  // 문자열 value 저장
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); // 문자열 key 저장
+        redisTemplate.setValueSerializer(new StringRedisSerializer()); // 문자열 value 저장
 
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
