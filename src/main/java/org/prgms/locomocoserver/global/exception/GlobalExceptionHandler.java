ba9e738a5d0b1d7005b78da18c4ab6ef5caff2fe
ZZAMBAs <rksidksrksi@naver.com>
2024-04-07 00:07:57 +0900

    feat #178: 예외 핸들러에 해당 기능 추가

--- src/main/java/org/prgms/locomocoserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/prgms/locomocoserver/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package org.prgms.locomocoserver.global.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.prgms.locomocoserver.global.common.SlackAlertService;
+import org.prgms.locomocoserver.global.common.dto.SlackErrorAlertDto;
 import org.prgms.locomocoserver.image.exception.ImageException;
 import org.prgms.locomocoserver.mogakkos.exception.MogakkoException;
 import org.prgms.locomocoserver.review.exception.ReviewException;
@@ -9,28 +13,51 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+    private final SlackAlertService slackAlertService;
 
     @ExceptionHandler(UserException.class)
-    public ResponseEntity<Object> handleUserException(UserException ex) {
+    public ResponseEntity<Object> handleUserException(UserException ex, HttpServletRequest request) {
+        slackAlertService.sendAlertLog(
+            new SlackErrorAlertDto(request.getRequestURL().toString(), request.getMethod(),
+                ex.getErrorType().getMessage()));
         return ResponseEntity.status(ex.getErrorType().getStatus()).body(ex.getErrorType().getMessage());
     }
 
     @ExceptionHandler(ImageException.class)
-    public ResponseEntity<Object> handleImageException(ImageException ex) {
+    public ResponseEntity<Object> handleImageException(ImageException ex, HttpServletRequest request) {
+        slackAlertService.sendAlertLog(
+            new SlackErrorAlertDto(request.getRequestURL().toString(), request.getMethod(),
+                ex.getErrorType().getMessage()));
         return ResponseEntity.status(ex.getErrorType().getStatus()).body(ex.getErrorType().getMessage());
     }
 
     @ExceptionHandler(ReviewException.class)
-    public ResponseEntity<Object> handleReviewException(ReviewException ex) {
+    public ResponseEntity<Object> handleReviewException(ReviewException ex, HttpServletRequest request) {
+        slackAlertService.sendAlertLog(
+            new SlackErrorAlertDto(request.getRequestURL().toString(), request.getMethod(),
+                ex.getErrorType().getMessage()));
         return ResponseEntity.status(ex.getErrorType().getStatus()).body(ex.getErrorType().getMessage());
     }
 
     @ExceptionHandler(MogakkoException.class)
-    public ResponseEntity<?> handleMogakkoException(MogakkoException ex) {
-        log.error(ex.getErrorCode().getMessage());
-        return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(ex.getErrorCode().getMessage());
+    public ResponseEntity<?> handleMogakkoException(MogakkoException ex, HttpServletRequest request) {
+        slackAlertService.sendAlertLog(
+            new SlackErrorAlertDto(request.getRequestURL().toString(), request.getMethod(),
+                ex.getErrorType().getMessage()));
+        log.error(ex.getErrorType().getMessage());
+        return ResponseEntity.status(ex.getErrorType().getHttpStatus()).body(ex.getErrorType().getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleUnknownException(Exception ex, HttpServletRequest request) {
+        slackAlertService.sendAlertLog(
+            new SlackErrorAlertDto(request.getRequestURL().toString(), request.getMethod(),
+                ex.getMessage()));
+        log.error("알 수 없는 에러 발생: {}", ex.getMessage());
+        return ResponseEntity.status(500).body(ex.getMessage());
     }
 }
