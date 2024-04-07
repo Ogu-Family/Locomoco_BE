f2f2060c5dca40ce73922ba9e165700b1ae046c6
eugene <81062608+eugene225@users.noreply.github.com>
2024-04-07 22:26:35 +0900

    Revert "feat #180 : 기존 코드 메서드 분리 및 필터 적용 api 패턴 선언"
    This reverts commit 51e4f02450636d2cd9b2e66ddba0f3635e2bc23a.

--- src/main/java/org/prgms/locomocoserver/global/filter/AuthenticationFilter.java
+++ src/main/java/org/prgms/locomocoserver/global/filter/AuthenticationFilter.java
@@ -5,14 +5,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.prgms.locomocoserver.global.exception.AuthException;
+import org.prgms.locomocoserver.global.exception.ErrorCode;
+import org.prgms.locomocoserver.global.exception.ExpiredTokenException;
+import org.prgms.locomocoserver.global.exception.InvalidTokenException;
 import org.prgms.locomocoserver.user.application.AuthenticationService;
+import org.prgms.locomocoserver.user.domain.enums.Provider;
+import org.prgms.locomocoserver.user.exception.UserErrorType;
+import org.prgms.locomocoserver.user.exception.UserException;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
 
 @Slf4j
 @Component
@@ -20,74 +25,55 @@ import java.util.Set;
 public class AuthenticationFilter implements Filter {
 
     private final AuthenticationService authenticationService;
-    private static final Set<String> allowedOrigin = new HashSet<>(Arrays.asList(
-            "http://localhost:3000",
-            "https://locomoco.kro.kr",
-            "https://locomoco.shop",
-            "http://localhost:8090"
-    ));
+    private static HashSet<String> alloweOrigin = new HashSet<>();
 
-    private static final Set<String> authRequired = new HashSet<>(Arrays.asList(
-            "GET:/api/v1/chats/rooms/\\d+",
-            "PATCH:/api/v1/mogakko/map/\\d+"
-    ));
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        alloweOrigin.add("http://localhost:3000");
+        alloweOrigin.add("https://locomoco.kro.kr");
+        alloweOrigin.add("https://locomoco.shop");
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
+        String accessToken = httpRequest.getHeader("Authorization");
+        String providerValue = httpRequest.getHeader("provider");
+
         if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
-            handleOptionCors(httpRequest, httpResponse);
+            String origin = httpRequest.getHeader("Origin") == null ? "https://locomoco.kro.kr" : httpRequest.getHeader("Origin");
+            log.info("CorsFilter Origin : " + origin);
+            if(alloweOrigin.contains(origin) == false) return;
+            httpResponse.setHeader("Access-Control-Allow-Origin", origin);
+            httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+            httpResponse.setHeader("Access-Control-Allow-Headers", "Authorization, provider");
+            httpResponse.setHeader("Access-Control-Expose-Headers", "Authorization, provider");  // 이건 있어야 하는지 확인
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
             return;
         }
 
-        String method = httpRequest.getMethod();
-        String url = httpRequest.getRequestURI();
+        if (accessToken == null || providerValue == null) {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
 
-        if (authRequired.stream().anyMatch(pattern -> isPatternMatch(pattern, method, url))) {
-            String accessToken = httpRequest.getHeader("Authorization");
-            String providerValue = httpRequest.getHeader("provider");
+        try {
+            boolean isValidToken = authenticationService.authenticateUser(providerValue, accessToken);
 
-            if (accessToken == null || providerValue == null) {
+            if (!isValidToken) {
+                log.info("AuthenticationFilter.doFilter !isValidToken called");
                 httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 return;
             }
 
-            try {
-                boolean isValidToken = authenticationService.authenticateUser(providerValue, accessToken);
-
-                if (!isValidToken) {
-                    log.info("AuthenticationFilter.doFilter !isValidToken called");
-                    httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                    return;
-                }
-
-            } catch (RuntimeException e) {
-                log.error("AuthenticationFilter - Exception: " + e.getMessage());
-                throw e; // 예외를 ExceptionHandlerFilter로 전달
-            }
+        } catch (RuntimeException e) {
+            log.error("AuthenticationFilter - Exception: " + e.getMessage());
+            throw e; // 예외를 ExceptionHandlerFilter로 전달
         }
 
         chain.doFilter(request, response);
-    }
-
-    private void handleOptionCors(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
-        String origin = httpRequest.getHeader("Origin") == null ? "https://locomoco.kro.kr" : httpRequest.getHeader("Origin");
-        log.info("CorsFilter Origin : " + origin);
-        if (!allowedOrigin.contains(origin)) return;
-        httpResponse.setHeader("Access-Control-Allow-Origin", origin);
-        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-        httpResponse.setHeader("Access-Control-Allow-Headers", "Authorization, provider");
-        httpResponse.setHeader("Access-Control-Expose-Headers", "Authorization, provider");
-        httpResponse.setStatus(HttpServletResponse.SC_OK);
-    }
-
-    private boolean isPatternMatch(String pattern, String method, String url) {
-        String[] parts = pattern.split(":");
-        String requestMethod = parts[0];
-        String requestUrl = parts[1];
-
-        return method.equalsIgnoreCase(requestMethod) && (url.equals(requestUrl) || url.matches(requestUrl.replace("*", "\\d*")));
     }
 }
