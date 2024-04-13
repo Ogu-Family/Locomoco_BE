ce95733cd8feeebea7241853e8437cae7e13aec8
eugene <81062608+eugene225@users.noreply.github.com>
2024-04-13 22:16:52 +0900

    feat #177 : mongoDB chatMessage 도큐먼트 구성

---
+++ src/main/java/org/prgms/locomocoserver/chat/domain/ChatMessageMongo.java
@@ -0,0 +1,28 @@
+package org.prgms.locomocoserver.chat.domain;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Document
+public class ChatMessageMongo {
+
+    @Id
+    private String id;
+    private String senderId;
+    private String senderNickname;
+    private String senderImage;
+    private String message;
+    private long timestamp;
+
+    @Builder
+    public ChatMessageMongo(String senderId, String senderNickname, String senderImage, String message, long timestamp) {
+        this.senderId = senderId;
+        this.senderNickname = senderNickname;
+        this.senderImage = senderImage;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
