82f9632ba400964ec65638fc02075e8eed0b4389
eugene <81062608+eugene225@users.noreply.github.com>
2024-04-13 22:16:06 +0900

    feat #177 : mongoDB chatRoom 컬렉션 구성

---
+++ src/main/java/org/prgms/locomocoserver/chat/domain/ChatRoomMongo.java
@@ -0,0 +1,29 @@
+package org.prgms.locomocoserver.chat.domain;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import org.prgms.locomocoserver.global.common.BaseEntity;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Document(collection = "chat_rooms")
+public class ChatRoomMongo {
+
+    @Id
+    private String id;
+    private List<ChatMessageMongo> messages;
+
+    public void addChatMessage(ChatMessageMongo chatMessage) {
+        if(chatMessage == null) throw new IllegalArgumentException();
+
+        this.messages.add(chatMessage);
+    }
+
+    @Builder
+    public ChatRoomMongo(List<ChatMessageMongo> messages) {
+        this.messages = messages;
+    }
+}
