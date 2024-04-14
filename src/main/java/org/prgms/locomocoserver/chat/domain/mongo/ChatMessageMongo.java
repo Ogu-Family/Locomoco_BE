60c5f2d27b94e2dd2b5acbaf2576d74586fbccdb
eugene <81062608+eugene225@users.noreply.github.com>
2024-04-15 02:30:37 +0900

    feat #177 : datetime 형식 변경

--- src/main/java/org/prgms/locomocoserver/chat/domain/mongo/ChatMessageMongo.java
+++ src/main/java/org/prgms/locomocoserver/chat/domain/mongo/ChatMessageMongo.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Document
 public class ChatMessageMongo {
@@ -15,14 +17,16 @@ public class ChatMessageMongo {
     private String senderNickname;
     private String senderImage;
     private String message;
-    private long timestamp;
+    private boolean isNotice;
+    private LocalDateTime createdAt;
 
     @Builder
-    public ChatMessageMongo(String senderId, String senderNickname, String senderImage, String message, long timestamp) {
+    public ChatMessageMongo(String senderId, String senderNickname, String senderImage, String message, boolean isNotice, LocalDateTime createdAt) {
         this.senderId = senderId;
         this.senderNickname = senderNickname;
         this.senderImage = senderImage;
         this.message = message;
-        this.timestamp = timestamp;
+        this.isNotice = isNotice;
+        this.createdAt = createdAt;
     }
 }
