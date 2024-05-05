c35d75c6d95155c0a51d07489b015783a6a64671
eugene <81062608+eugene225@users.noreply.github.com>
2024-05-05 17:24:39 +0900

    feat #177 : mongoDB 컬렉션 생성 기존 서비스 코드에 추가

--- src/main/java/org/prgms/locomocoserver/chat/application/ChatRoomService.java
+++ src/main/java/org/prgms/locomocoserver/chat/application/ChatRoomService.java
@@ -51,7 +51,9 @@ public class ChatRoomService {
                 .chatRoom(chatRoom).build());
 
         chatRoom.addChatParticipant(chatParticipant);
-        chatRoomRepository.save(chatRoom);
+        chatRoomRepository.save(chatRoom); // TODO : mysql chat room create
+        mongoChatMessageService.createChatRoom(chatRoom.getId()); // TODO : mongo chat room create
+        
         mySqlChatMessageService.saveEnterMessage(chatRoom.getId(), chatParticipant.getUser());  // TODO : mysql chat
         mongoChatMessageService.saveEnterMessage(chatRoom.getId(), requestDto.creator()); // TODO : mongo chat
 
