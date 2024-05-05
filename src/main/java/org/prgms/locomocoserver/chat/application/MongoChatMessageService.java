4e084ddc4c6ca32f1837a78136758bc14b7802b7
eugene <81062608+eugene225@users.noreply.github.com>
2024-05-05 16:45:12 +0900

    feat #177 : 채팅방 메시지 조회, 삭제 메서드 추가

--- src/main/java/org/prgms/locomocoserver/chat/application/MongoChatMessageService.java
+++ src/main/java/org/prgms/locomocoserver/chat/application/MongoChatMessageService.java
@@ -10,12 +10,19 @@ import org.prgms.locomocoserver.chat.exception.ChatErrorType;
 import org.prgms.locomocoserver.chat.exception.ChatException;
 import org.prgms.locomocoserver.user.application.UserService;
 import org.prgms.locomocoserver.user.domain.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.mongodb.core.query.Query;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -37,6 +44,7 @@ public class MongoChatMessageService implements ChatMessagePolicy {
         return chatRoomRepository.findById(roomId).orElseThrow(() -> new ChatException(ChatErrorType.CHATROOM_NOT_FOUND));
     }
 
+    @Transactional
     public ChatMessageDto saveEnterMessage(Long roomId, User sender) {
         String collectionName = BASE_CHATROOM_NAME + roomId;
         ChatMessageMongo chatMessageMongo = mongoTemplate.save(toEnterMessage(sender), collectionName);
@@ -44,6 +52,7 @@ public class MongoChatMessageService implements ChatMessagePolicy {
         return ChatMessageDto.of(roomId, chatMessageMongo);
     }
 
+    @Transactional
     public ChatMessageDto saveChatMessage(Long roomId, ChatMessageRequestDto message) {
         String collectionName = BASE_CHATROOM_NAME + roomId;
         User participant = userService.getById(message.senderId());
@@ -54,15 +63,40 @@ public class MongoChatMessageService implements ChatMessagePolicy {
         return ChatMessageDto.of(roomId, chatMessageMongo);
     }
 
-    public List<ChatMessageDto> getAllChatMessages(Long roomId, String cursor, int pageSize) {
-        String collectionName = BASE_CHATROOM_NAME + roomId;
-        return mongoTemplate.findAll(ChatMessageMongo.class, collectionName)
-                .stream().map(chatMessageMongo -> ChatMessageDto.of(roomId, chatMessageMongo)).toList();
+    @Transactional(readOnly = true)
+    public List<ChatMessageDto> getAllChatMessages(Long roomId, String cursorValue, int pageSize) {
+        String collectionName = getChatRoomName(roomId);
+        Query query = new Query();
+
+        if (cursorValue != "null") {
+            query.addCriteria(Criteria.where("_id").gt(cursorValue));
+        }
+
+        query.with(Sort.by(Sort.Direction.DESC, "_id")).limit(pageSize);
+
+        List<ChatMessageMongo> chatMessages = mongoTemplate.find(query, ChatMessageMongo.class, collectionName);
+
+        List<ChatMessageDto> chatMessageDtos = chatMessages.stream()
+                .map(chatMessageMongo -> ChatMessageDto.of(roomId, chatMessageMongo))
+                .collect(Collectors.toList());
+
+        return chatMessageDtos;
     }
 
-    @Override
-    public void deleteChatMessages(Long roomId) {
+    @Transactional
+    public void deleteChatMessages(ChatRoom chatRoom) {
+        String collectionName = getChatRoomName(chatRoom.getId());
+        mongoTemplate.dropCollection(collectionName);
+    }
 
+    @Transactional(readOnly = true)
+    public ChatMessageDto getLastChatMessage(Long roomId) {
+        String collectionName = getChatRoomName(roomId);
+
+        Query query = new Query().with(Sort.by(Sort.Direction.DESC, "_id")).limit(1);
+        ChatMessageMongo lastMessage = mongoTemplate.findOne(query, ChatMessageMongo.class, collectionName);
+
+        return ChatMessageDto.of(roomId, lastMessage);
     }
 
     public String getChatRoomName(Long roomId) {
