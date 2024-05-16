be326116c43dd0163562527cb5c160042df47780
ZZAMBAs <rksidksrksi@naver.com>
2024-05-17 00:27:20 +0900

    fix: Redis TLS 연결 부분 삭제

--- src/main/java/org/prgms/locomocoserver/global/config/RedisConfig.java
+++ src/main/java/org/prgms/locomocoserver/global/config/RedisConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -33,8 +32,10 @@ public class RedisConfig {
         redisStandaloneConfiguration.setPort(redisPort);
         redisStandaloneConfiguration.setPassword(redisPassword);
 
-        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder().useSsl().build();
-        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfiguration);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+
+        /*LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder().useSsl().build();
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfiguration);*/
     }
 
     @Bean
