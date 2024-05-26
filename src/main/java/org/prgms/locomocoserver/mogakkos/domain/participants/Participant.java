e5b447c801f4ce80fdce9c8f7e44b750b4ca2b92
ZZAMBAs <rksidksrksi@naver.com>
2024-05-26 16:53:11 +0900

    feat #187: 참여자들 위치를 가지도록 수정

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/participants/Participant.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/participants/Participant.java
@@ -1,5 +1,6 @@
 package org.prgms.locomocoserver.mogakkos.domain.participants;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +27,12 @@ public class Participant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "latitude", columnDefinition = "decimal(13, 10)")
+    private Double latitude;
+
+    @Column(name = "longitude", columnDefinition = "decimal(13, 10)")
+    private Double longitude;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
@@ -35,7 +42,9 @@ public class Participant {
     private Mogakko mogakko;
 
     @Builder
-    public Participant(User user, Mogakko mogakko) {
+    public Participant(Double latitude, Double longitude, User user, Mogakko mogakko) {
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.user = user;
         this.mogakko = mogakko;
     }
