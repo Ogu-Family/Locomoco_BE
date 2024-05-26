aa8f4fd8b55fe68d754160abcf5fb05c944d0032
ZZAMBAs <rksidksrksi@naver.com>
2024-05-26 16:51:07 +0900

    feat #187: 도메인 수정

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/location/MogakkoLocation.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/location/MogakkoLocation.java
@@ -1,9 +1,12 @@
 package org.prgms.locomocoserver.mogakkos.domain.location;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -21,11 +24,16 @@ public class MogakkoLocation extends Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mogakko_id")
+    protected Mogakko mogakko;
+
     @Builder
     public MogakkoLocation(double latitude, double longitude, String address, String city,
         Mogakko mogakko, Long id) {
-        super(latitude, longitude, address, city, mogakko);
+        super(latitude, longitude, address, city);
         this.id = id;
+        this.mogakko = mogakko;
     }
 
     public void updateInfo(double latitude, double longitude, String address, String city) {
--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/midpoint/Midpoint.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/midpoint/Midpoint.java
@@ -1,13 +1,17 @@
 package org.prgms.locomocoserver.mogakkos.domain.midpoint;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.prgms.locomocoserver.mogakkos.domain.Location;
+import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
 
 @Entity
 @Getter
@@ -17,5 +21,7 @@ public class Midpoint extends Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mogakko_id")
+    protected Mogakko mogakko;
 }
