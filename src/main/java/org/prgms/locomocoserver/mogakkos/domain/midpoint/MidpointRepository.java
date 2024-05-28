9ef05ded00b278df302559e127fa0472185c6e89
ZZAMBAs <rksidksrksi@naver.com>
2024-05-28 23:33:35 +0900

    feat #187: 모각코 ID로 엔티티 찾는 기능 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/midpoint/MidpointRepository.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/midpoint/MidpointRepository.java
@@ -1,7 +1,8 @@
 package org.prgms.locomocoserver.mogakkos.domain.midpoint;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MidpointRepository extends JpaRepository<Midpoint, Long> {
-
+    Optional<Midpoint> findByMogakkoId(Long mogakkoId);
 }
