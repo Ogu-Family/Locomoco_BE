c4a9bb77a6aa39b296de8974cced9c8fdb4003b1
eugene <81062608+eugene225@users.noreply.github.com>
2024-05-30 00:53:20 +0900

    test #199 : device key 서비스 로직 테스트 코드 작성

---
+++ src/test/java/org/prgms/locomocoserver/user/application/DeviceKeyServiceTest.java
@@ -0,0 +1,73 @@
+package org.prgms.locomocoserver.user.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.prgms.locomocoserver.user.domain.mongo.DeviceKeyMongo;
+import org.prgms.locomocoserver.user.domain.mongo.DeviceKeyMongoRepository;
+import org.prgms.locomocoserver.user.dto.request.DeviceKeyUpdateRequest;
+import org.prgms.locomocoserver.user.dto.response.DeviceKeyDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DeviceKeyServiceTest {
+
+    @Autowired
+    private DeviceKeyMongoRepository deviceKeyMongoRepository;
+
+    @Autowired
+    private DeviceKeyService deviceKeyService;
+
+    @BeforeEach
+    void setUp() {
+        deviceKeyMongoRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("userId에 맞는 디바이스 토큰을 저장할 수 있다.")
+    void saveDeviceKey() {
+        // given
+
+        // when
+        DeviceKeyDto deviceKey = deviceKeyService.saveDeviceKey(String.valueOf(2L));
+
+        // then
+        assertThat(deviceKey.pad()).isEqualTo(null);
+        assertThat(deviceKey.phone()).isEqualTo(null);
+        assertThat(deviceKey.desktop()).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("userId로 디바이스 토큰을 조회할 수 있다.")
+    void getByUserId() {
+        // given
+        DeviceKeyMongo deviceKeyMongo = deviceKeyMongoRepository.save(DeviceKeyMongo.builder()
+                .userId("2").desktop("desktop test").pad("pad test").phone("phone test").build());
+
+        // when
+        DeviceKeyDto deviceKey = deviceKeyService.getByUserId("2");
+
+        // then
+        assertThat(deviceKey.desktop()).isEqualTo(deviceKeyMongo.getDesktop());
+        assertThat(deviceKey.pad()).isEqualTo(deviceKeyMongo.getPad());
+        assertThat(deviceKey.phone()).isEqualTo(deviceKeyMongo.getPhone());
+    }
+
+    @Test
+    @DisplayName("userId, 디바이스 타입으로 token 값을 업데이트 할 수 있다.")
+    void updateDeviceKey() {
+        // given
+        DeviceKeyMongo deviceKeyMongo = deviceKeyMongoRepository.save(DeviceKeyMongo.builder()
+                .userId("2").desktop("desktop test").pad("pad test").phone("phone test").build());
+        DeviceKeyUpdateRequest request = new DeviceKeyUpdateRequest("phone", "update");
+
+        // when
+        DeviceKeyDto deviceKeyDto = deviceKeyService.updateDeviceKey("2", request);
+
+        // then
+        assertThat(deviceKeyDto.phone()).isEqualTo("update");
+    }
+}
