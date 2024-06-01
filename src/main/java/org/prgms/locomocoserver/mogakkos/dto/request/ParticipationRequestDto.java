09ff82e16eb725d513a5d39dc8cbbfb9c2bc0627
ZZAMBAs <rksidksrksi@naver.com>
2024-06-02 00:31:27 +0900

    feat #203: userId만 받는 생성자 추가

--- src/main/java/org/prgms/locomocoserver/mogakkos/dto/request/ParticipationRequestDto.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/dto/request/ParticipationRequestDto.java
@@ -6,4 +6,7 @@ public record ParticipationRequestDto(@Schema(description = "참여하고자 하
                                       @Schema(description = "참여하고자 하는 유저의 출발 경도", example = "126.97335352452") Double longitude,
                                       @Schema(description = "참여하고자 하는 유저의 출발 위도", example = "26.83783263") Double latitude) {
 
+    public ParticipationRequestDto(Long userId) {
+        this(userId, null, null);
+    }
 }
