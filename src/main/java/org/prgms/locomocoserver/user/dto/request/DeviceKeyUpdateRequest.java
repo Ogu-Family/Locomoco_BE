8627761503049f0dcf391343d0508262e7eeb9fc
eugene <81062608+eugene225@users.noreply.github.com>
2024-06-02 23:04:02 +0900

    fix : 프론트 dto 타입 수정

--- src/main/java/org/prgms/locomocoserver/user/dto/request/DeviceKeyUpdateRequest.java
+++ src/main/java/org/prgms/locomocoserver/user/dto/request/DeviceKeyUpdateRequest.java
@@ -1,6 +1,7 @@
 package org.prgms.locomocoserver.user.dto.request;
 
 public record DeviceKeyUpdateRequest(
+        String id, // 프론트 타입 이유로 userId 값 추가
         String deviceType,
         String token
 ) {
