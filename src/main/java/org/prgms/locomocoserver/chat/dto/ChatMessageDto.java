5775af50635680bd1debc7a4dcd4c955a8bafdb0
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-03 14:41:05 +0900

    refactor #210 : null 처리 코드 수정

--- src/main/java/org/prgms/locomocoserver/chat/dto/ChatMessageDto.java
+++ src/main/java/org/prgms/locomocoserver/chat/dto/ChatMessageDto.java
@@ -18,19 +18,14 @@ public record ChatMessageDto(
 ) {
     public static ChatMessageDto of(ChatMessage chatMessage) {
         User sender = chatMessage.getSender();
-        String profileImagePath = null;
-        if (sender.getProfileImage() != null) {
-            profileImagePath = sender.getProfileImage().getPath();
-        }
+        String profileImagePath = sender.getProfileImage() == null ? null : sender.getProfileImage().getPath();
+
         return new ChatMessageDto(chatMessage.getId().toString(), chatMessage.getChatRoom().getId().toString(), sender.getId().toString(),
                 sender.getNickname(), profileImagePath, chatMessage.getContent(), chatMessage.isNotice(), chatMessage.getCreatedAt());
     }
 
     public static ChatMessageDto of(Long chatRoomId, ChatMessageMongo chatMessageMongo) {
-        String profileImagePath = null;
-        if (chatMessageMongo.getSenderImage() != null) {
-            profileImagePath = chatMessageMongo.getSenderImage();
-        }
+        String profileImagePath = chatMessageMongo.getSenderImage() == null ? null : chatMessageMongo.getSenderImage();
         return new ChatMessageDto(chatMessageMongo.getId(), chatRoomId.toString(), chatMessageMongo.getSenderId(),
                 chatMessageMongo.getSenderNickname(), profileImagePath, chatMessageMongo.getMessage(), chatMessageMongo.isNotice(), chatMessageMongo.getCreatedAt());
     }
