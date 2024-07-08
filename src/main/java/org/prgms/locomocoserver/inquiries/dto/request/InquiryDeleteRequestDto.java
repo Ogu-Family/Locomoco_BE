1d6c5bd5f066e73e8f0c1095f0a247707289045e
ZZAMBAs <rksidksrksi@naver.com>
2024-07-08 16:56:41 +0900

    remove #72: 사용하지 않는 DTO 삭제

--- src/main/java/org/prgms/locomocoserver/inquiries/dto/request/InquiryDeleteRequestDto.java
+++
@@ -1,7 +0,0 @@
-package org.prgms.locomocoserver.inquiries.dto.request;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-public record InquiryDeleteRequestDto(@Schema(description = "작성자 id", example = "1") Long userId) {
-
-}
