4e887eda892e20520872ab66cc461bcac26adf49
ZZAMBAs <rksidksrksi@naver.com>
2024-07-08 14:13:54 +0900

    feat #72: 문의 도메인 검증 추가

--- src/main/java/org/prgms/locomocoserver/inquiries/domain/Inquiry.java
+++ src/main/java/org/prgms/locomocoserver/inquiries/domain/Inquiry.java
@@ -22,6 +22,7 @@ import org.prgms.locomocoserver.user.domain.User;
 @Table(name = "inquiries")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Inquiry extends BaseEntity {
+    private static final int MAXIMUM_CONTENT_LENGTH = 200;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,12 +41,22 @@ public class Inquiry extends BaseEntity {
 
     @Builder
     public Inquiry(String content, User user, Mogakko mogakko) {
+        validateInfo(content);
+
         this.content = content;
         this.user = user;
         this.mogakko = mogakko;
     }
 
     public void updateInfo(String content) {
+        validateInfo(content);
+
         this.content = content;
+    }
+
+    private static void validateInfo(String content) {
+        if (content.length() > MAXIMUM_CONTENT_LENGTH) {
+            throw new RuntimeException("문의 내용은 " + MAXIMUM_CONTENT_LENGTH + "자를 초과할 수 없습니다."); // TODO: 문의 예외 반환
+        }
     }
 }
