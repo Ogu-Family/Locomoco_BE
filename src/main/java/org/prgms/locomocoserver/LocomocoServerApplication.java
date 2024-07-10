5c78a383bdabc26a597d53afbe165e67cdb63dfa
ZZAMBAs <rksidksrksi@naver.com>
2024-07-11 00:35:34 +0900

    refactor #25: 기본 스프링 실행 클래스와 임의 어노테이션 분리
    - 스프링 공식 문서 참조: https://docs.spring.io/spring-boot/reference/testing/spring-boot-applications.html#testing.spring-boot-applications.user-configuration-and-slicing

--- src/main/java/org/prgms/locomocoserver/LocomocoServerApplication.java
+++ src/main/java/org/prgms/locomocoserver/LocomocoServerApplication.java
@@ -2,10 +2,8 @@ package org.prgms.locomocoserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class LocomocoServerApplication {
 
     public static void main(String[] args) {
