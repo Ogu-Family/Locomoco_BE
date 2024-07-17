e56e695bbe85ea0759e94b9bcd204c25d10cf7d7
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-17 14:18:09 +0900

    feat #218 : 댓글 신고 조회 메서드 추가

--- src/main/java/org/prgms/locomocoserver/report/domain/ReplyReportRepository.java
+++ src/main/java/org/prgms/locomocoserver/report/domain/ReplyReportRepository.java
@@ -3,6 +3,8 @@ package org.prgms.locomocoserver.report.domain;
 import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReplyReportRepository extends JpaRepository<ReplyReport, Id> {
+import java.util.Optional;
 
+public interface ReplyReportRepository extends JpaRepository<ReplyReport, Id> {
+    Optional<ReplyReport> findById(Long id);
 }
