6621e69295d80bf4d18877dc6d01074b61c881df
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-17 14:17:52 +0900

    feat #218 : 댓글신고 수정 서비스 코드 구현

--- src/main/java/org/prgms/locomocoserver/report/application/ReplyReportService.java
+++ src/main/java/org/prgms/locomocoserver/report/application/ReplyReportService.java
@@ -5,13 +5,16 @@ import org.prgms.locomocoserver.replies.domain.Reply;
 import org.prgms.locomocoserver.replies.domain.ReplyRepository;
 import org.prgms.locomocoserver.report.domain.ReplyReport;
 import org.prgms.locomocoserver.report.domain.ReplyReportRepository;
+import org.prgms.locomocoserver.report.domain.Report;
 import org.prgms.locomocoserver.report.dto.ReplyReportDto;
 import org.prgms.locomocoserver.report.dto.request.ReplyReportCreateRequest;
+import org.prgms.locomocoserver.report.dto.request.ReplyReportUpdateRequest;
 import org.prgms.locomocoserver.report.exception.ReportErrorType;
 import org.prgms.locomocoserver.report.exception.ReportException;
 import org.prgms.locomocoserver.user.application.UserService;
 import org.prgms.locomocoserver.user.domain.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +31,18 @@ public class ReplyReportService {
         ReplyReport replyReport = replyReportRepository.save(request.toEntity(reporter, reply, request.content()));
 
         return ReplyReportDto.of(replyReport);
+    }
+
+    @Transactional
+    public ReplyReportDto update(Long id, ReplyReportUpdateRequest request) {
+        ReplyReport replyReport = getById(id);
+        replyReport.updateContent(request.content());
+
+        return ReplyReportDto.of(replyReport);
+    }
+
+    private ReplyReport getById(Long id) {
+        return replyReportRepository.findById(id)
+                .orElseThrow(() -> new ReportException(ReportErrorType.REPORT_NOT_FOUND));
     }
 }
