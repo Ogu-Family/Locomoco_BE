21f4f89c2213b6934034c699a14cc81c0cdcf02f
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-17 14:34:40 +0900

    feat #218 : 댓글 신고 페이지네이션 조회 메서드 및 쿼리 추가

--- src/main/java/org/prgms/locomocoserver/report/domain/ReplyReportRepository.java
+++ src/main/java/org/prgms/locomocoserver/report/domain/ReplyReportRepository.java
@@ -2,9 +2,13 @@ package org.prgms.locomocoserver.report.domain;
 
 import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReplyReportRepository extends JpaRepository<ReplyReport, Id> {
     Optional<ReplyReport> findById(Long id);
+    @Query(value = "SELECT * FROM  reply_reports r WHERE r.deleted_at IS NULL AND r.id > :cursor ORDER BY r.id limit :pageSize", nativeQuery = true)
+    List<ReplyReport> findAllByDeletedAtIsNull(Long cursor, int pageSize);
 }
