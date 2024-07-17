2334b9453e3ac6c5acb0aa6be4ab1c193c08effc
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-18 00:47:04 +0900

    fix #143 : conflict 해결

--- src/main/java/org/prgms/locomocoserver/chat/dto/request/ChatMessageRequestDto.java
+++ src/main/java/org/prgms/locomocoserver/chat/dto/request/ChatMessageRequestDto.java
@@ -16,7 +16,8 @@ public record ChatMessageRequestDto(
         @Nonnull
         Long senderId,
         @NotBlank @Length(max = 255, message = "글자수는 255자까지 입력 가능합니다.")
-        String message
+        String message,
+        String imageByteCode
 ) {
     public ChatMessage toChatMessageEntity(User sender, ChatRoom chatRoom, boolean isNotice) {
         return ChatMessage.builder()
@@ -28,12 +29,20 @@ public record ChatMessageRequestDto(
 
     public ChatMessageMongo toChatMessageMongo(User sender, ChatRoom chatRoom, boolean isNotice) {
         return ChatMessageMongo.builder()
-               .senderId(senderId.toString())
-               .senderNickname(sender.getNickname())
-               .senderImage(sender.getProfileImage().getPath())
-               .message(message)
-               .isNotice(isNotice)
-               .createdAt(LocalDateTime.now())
-               .build();
+                .senderId(senderId.toString())
+                .senderNickname(sender.getNickname())
+                .senderImage(sender.getProfileImage().getPath())
+                .message(message)
+                .isNotice(isNotice)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public ChatMessage toChatImageMessage(User sender, ChatRoom chatRoom) {
+        return ChatMessage.builder()
+                .sender(sender)
+                .chatRoom(chatRoom)
+                .content("IMAGE")
+                .isNotice(false).build();
     }
 }
