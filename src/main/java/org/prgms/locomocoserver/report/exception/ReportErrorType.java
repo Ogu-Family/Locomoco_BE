1dc75138e30804b72d128ee0ca3b9ea67fd110e3
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-17 14:35:30 +0900

    feat #218 : 수정시 권한 확인 에러타입 추가

--- src/main/java/org/prgms/locomocoserver/report/exception/ReportErrorType.java
+++ src/main/java/org/prgms/locomocoserver/report/exception/ReportErrorType.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public enum ReportErrorType {
     REPLY_NOT_FOUND(HttpStatus.NOT_FOUND, "신고할 댓글을 찾을 수 없습니다."),
-    REPORT_NOT_FOUND(HttpStatus.NOT_FOUND, "신고를 찾을 수 없습니다.")
+    REPORT_NOT_FOUND(HttpStatus.NOT_FOUND, "신고를 찾을 수 없습니다."),
+    AUTH_NOT_ALLOWED(HttpStatus.UNAUTHORIZED, "작성자만 수정가능합니다.")
     ;
 
     private final HttpStatus httpStatus;
