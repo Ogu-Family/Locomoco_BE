bf03301d49cae4ebe8d36b5e2206178d66cf213f
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-17 14:35:06 +0900

    refactor #218 : update dto reporterId 추가

--- src/main/java/org/prgms/locomocoserver/report/dto/request/ReplyReportUpdateRequest.java
+++ src/main/java/org/prgms/locomocoserver/report/dto/request/ReplyReportUpdateRequest.java
@@ -1,8 +1,11 @@
 package org.prgms.locomocoserver.report.dto.request;
 
+import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.NotBlank;
 
 public record ReplyReportUpdateRequest(
+        @Nonnull
+        Long reporterId,
         @NotBlank
         String content
 ) {
