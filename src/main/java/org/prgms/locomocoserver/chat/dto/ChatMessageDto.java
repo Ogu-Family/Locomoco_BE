75fcd6f8c0158b35d1115e88abcc93c057a4be27
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-18 17:36:36 +0900

    feat #143 : entity, dto에 메시지 이미지 필드 추가

--- src/main/java/org/prgms/locomocoserver/chat/dto/ChatMessageDto.java
+++ src/main/java/org/prgms/locomocoserver/chat/dto/ChatMessageDto.java
@@ -5,6 +5,7 @@ import org.prgms.locomocoserver.chat.domain.mongo.ChatMessageMongo;
 import org.prgms.locomocoserver.user.domain.User;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record ChatMessageDto(
         String chatMessageId,
@@ -13,6 +14,7 @@ public record ChatMessageDto(
         String senderNickName,
         String senderProfileImage,
         String message,
+        List<String> imageUrls,
         boolean isNotice,
         LocalDateTime createdAt
 ) {
@@ -21,13 +23,19 @@ public record ChatMessageDto(
         String profileImagePath = sender.getProfileImage() == null ? null : sender.getProfileImage().getPath();
 
         return new ChatMessageDto(chatMessage.getId().toString(), chatMessage.getChatRoom().getId().toString(), sender.getId().toString(),
-                sender.getNickname(), profileImagePath, chatMessage.getContent(), chatMessage.isNotice(), chatMessage.getCreatedAt());
+                sender.getNickname(), profileImagePath, chatMessage.getContent(), null, chatMessage.isNotice(), chatMessage.getCreatedAt());
     }
 
     public static ChatMessageDto of(Long chatRoomId, ChatMessageMongo chatMessageMongo) {
         String profileImagePath = chatMessageMongo.getSenderImage() == null ? null : chatMessageMongo.getSenderImage();
         return new ChatMessageDto(chatMessageMongo.getId(), chatRoomId.toString(), chatMessageMongo.getSenderId(),
-                chatMessageMongo.getSenderNickname(), profileImagePath, chatMessageMongo.getMessage(), chatMessageMongo.isNotice(), chatMessageMongo.getCreatedAt());
+                chatMessageMongo.getSenderNickname(), profileImagePath, chatMessageMongo.getMessage(), null, chatMessageMongo.isNotice(), chatMessageMongo.getCreatedAt());
+    }
+
+    public static ChatMessageDto of(Long chatRoomId, List<String> imageUrls, ChatMessageMongo chatMessageMongo) {
+        String profileImagePath = chatMessageMongo.getSenderImage() == null? null : chatMessageMongo.getSenderImage();
+        return new ChatMessageDto(chatMessageMongo.getId(), chatRoomId.toString(), chatMessageMongo.getSenderId(),
+                chatMessageMongo.getSenderNickname(), profileImagePath, chatMessageMongo.getMessage(), imageUrls, chatMessageMongo.isNotice(), chatMessageMongo.getCreatedAt());
     }
 }
 
