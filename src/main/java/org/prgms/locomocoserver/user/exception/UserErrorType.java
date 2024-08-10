f35a200b71b2554467c4d7d08571d9790c2036ff
eugene <81062608+eugene225@users.noreply.github.com>
2024-08-10 17:55:57 +0900

    refactor #228 : 사용자 나이 에러타입 추가

--- src/main/java/org/prgms/locomocoserver/user/exception/UserErrorType.java
+++ src/main/java/org/prgms/locomocoserver/user/exception/UserErrorType.java
@@ -8,6 +8,7 @@ public enum UserErrorType {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자가 존재하지 않습니다."),
     PROVIDER_TYPE_ERROR(HttpStatus.UNAUTHORIZED, "해당 provider가 존재하지 않습니다."),
     BIRTH_TYPE_ERROR(HttpStatus.BAD_REQUEST, "생년월일이 형식에 맞지 않습니다."),
+    AGE_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "만 12세 이상만 가입 가능합니다."),
     EMAIL_TYPE_ERROR(HttpStatus.BAD_REQUEST, "이메일이 형식에 맞지 않습니다."),
     TEMPERATURE_TYPE_ERROR(HttpStatus.BAD_REQUEST, "온도는 0도 이상 100도 이하의 값이어야 합니다."),
     ACCESSTOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 엑세스 토큰이 존재하지 않습니다."),
