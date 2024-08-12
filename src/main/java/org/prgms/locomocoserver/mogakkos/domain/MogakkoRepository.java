1de79eff26bae108d8f9201c5600fbc32cfb27c6
ZZAMBAs <rksidksrksi@naver.com>
2024-08-12 16:38:31 +0900

    feat #226: OR에 맞게 쿼리 수정 및 쿼리 분리 (제목+내용, 닉네임, 장소)

--- src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
+++ src/main/java/org/prgms/locomocoserver/mogakkos/domain/MogakkoRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface MogakkoRepository extends JpaRepository<Mogakko, Long> {
 
@@ -19,46 +18,40 @@ public interface MogakkoRepository extends JpaRepository<Mogakko, Long> {
     @Query("SELECT m FROM Mogakko m WHERE m.id = :id AND m.deletedAt IS NULL")
     Optional<Mogakko> findByIdAndDeletedAtIsNullForUpdate(Long id); // 이 코드는 추후 따로 클래스 분리를 할 수도 있음
 
-    @Query(value = "SELECT m.* FROM mogakko m "
-        + "JOIN locations l ON (m.id < :cursor AND m.deadline > :now AND m.deleted_at IS NULL AND l.mogakko_id = m.id) "
-        + "WHERE MATCH(l.city) AGAINST(CONCAT('\"', :city, '\"') IN BOOLEAN MODE) "
-        + "ORDER BY m.created_at DESC "
-        + "LIMIT :pageSize", nativeQuery = true)
-    List<Mogakko> findAllByCity(Long cursor, String city, int pageSize, LocalDateTime now);
-
     @Query(value = "SELECT m.* "
-        + "FROM mogakko_tags mt "
-        + "INNER JOIN mogakko m ON m.id < :cursor AND m.deadline > :now AND m.deleted_at IS NULL AND m.id = mt.mogakko_id "
-        + "INNER JOIN locations l ON l.mogakko_id = m.id "
-        + "WHERE mt.tag_id IN :tagIds AND MATCH(l.city) AGAINST(CONCAT('\"', :city, '\"') IN BOOLEAN MODE) "
-        + "GROUP BY mt.mogakko_id HAVING COUNT(mt.mogakko_id) = :tagSize "
-        + "ORDER BY m.created_at DESC "
+        + "FROM mogakko m "
+        + "INNER JOIN locations l ON l.mogakko_id = m.id AND m.deadline > :searchTime "
+            + "AND m.deleted_at IS NULL AND (MATCH(l.city) AGAINST(:city IN BOOLEAN MODE) OR MATCH(l.h_city) AGAINST(:city IN BOOLEAN MODE)) "
+        + "LEFT JOIN mogakko_tags mt ON mt.tag_id IN :tagIds AND mt.mogakko_id = m.id "
+        + "GROUP BY m.id HAVING (COUNT(mt.id) = :countCursor AND m.created_at <= :timeCursor AND m.id < :cursorId) OR COUNT(mt.id) < :countCursor "
+        + "ORDER BY COUNT(mt.id) DESC, m.created_at DESC "
         + "LIMIT :pageSize", nativeQuery = true)
-    List<Mogakko> findAllByCity(Iterable<Long> tagIds, int tagSize, Long cursor, String city, int pageSize, LocalDateTime now);
+    List<Mogakko> findAllByCity(Iterable<Long> tagIds, String city, int pageSize, LocalDateTime searchTime, Long countCursor, LocalDateTime timeCursor, Long cursorId); // 장소
 
     @Query(value = "SELECT m.* FROM mogakko m "
-        + "INNER JOIN users u ON m.id < :cursor AND m.deleted_at IS NULL AND m.deadline > :now AND m.creator_id = u.id "
+        + "INNER JOIN users u ON m.deleted_at IS NULL AND m.deadline > :searchTime AND m.creator_id = u.id "
+            + "AND (MATCH(m.title) AGAINST(:searchVal IN BOOLEAN MODE) OR MATCH(m.content) AGAINST(:searchVal IN BOOLEAN MODE)) "
         + "INNER JOIN locations l ON l.mogakko_id = m.id "
-        + "INNER JOIN mogakko_tags mt ON mt.tag_id IN :tagIds AND mt.mogakko_id = m.id "
-        + "WHERE MATCH(m.title) AGAINST(:searchVal IN BOOLEAN MODE) OR MATCH(m.content) AGAINST(:searchVal IN BOOLEAN MODE) OR u.nickname LIKE :searchVal% OR MATCH(l.city) AGAINST(:searchVal IN BOOLEAN MODE) "
-        + "GROUP BY m.id HAVING count(m.id) = :tagSize "
-        + "ORDER BY m.created_at DESC "
+        + "LEFT JOIN mogakko_tags mt ON mt.tag_id IN :tagIds AND mt.mogakko_id = m.id "
+        + "GROUP BY m.id HAVING (COUNT(mt.id) = :countCursor AND m.created_at <= :timeCursor AND m.id < :cursorId) OR COUNT(mt.id) < :countCursor "
+        + "ORDER BY COUNT(m.id) DESC, m.created_at DESC, m.id DESC "
         + "LIMIT :pageSize", nativeQuery = true)
-    List<Mogakko> findAllByFilter(Long cursor, String searchVal, List<Long> tagIds, int tagSize, int pageSize, LocalDateTime now);
+    List<Mogakko> findAllByTitleAndContent(String searchVal, List<Long> tagIds, int pageSize, LocalDateTime searchTime, Long countCursor, LocalDateTime timeCursor, Long cursorId); // 제목 + 내용
 
     @Query(value = "SELECT m.* FROM mogakko m "
-        + "INNER JOIN users u ON m.id < :cursor AND m.deadline > :now AND m.deleted_at IS NULL AND m.creator_id = u.id "
-        + "INNER JOIN locations l ON l.mogakko_id = m.id "
-        + "WHERE MATCH(m.title) AGAINST(:searchVal IN BOOLEAN MODE) OR MATCH(m.content) AGAINST(:searchVal IN BOOLEAN MODE) OR u.nickname LIKE :searchVal% OR MATCH(l.city) AGAINST(:searchVal IN BOOLEAN MODE) "
-        + "ORDER BY m.created_at DESC "
+        + "JOIN users u ON m.deleted_at IS NULL AND m.deadline > :searchTime "
+            + "AND MATCH(u.nickname) AGAINST(:searchVal IN BOOLEAN MODE) AND m.creator_id = u.id "
+        + "LEFT JOIN mogakko_tags mt ON mt.mogakko_id = m.id AND mt.tag_id IN :tagIds "
+        + "GROUP BY m.id HAVING (COUNT(mt.id) = :countCursor AND m.created_at <= :timeCursor AND m.id < :cursorId) OR COUNT(mt.id) < :countCursor "
+        + "ORDER BY COUNT(m.id) DESC, m.created_at DESC, m.id DESC "
         + "LIMIT :pageSize", nativeQuery = true)
-    List<Mogakko> findAllByFilter(Long cursor, String searchVal, int pageSize, LocalDateTime now);
+    List<Mogakko> findAllByUserNickname(String searchVal, List<Long> tagIds, int pageSize, LocalDateTime searchTime, Long countCursor, LocalDateTime timeCursor, Long cursorId);
 
-    @Query("SELECT m FROM Mogakko m JOIN m.participants p WHERE p.user = :user AND m.endTime >= :now AND m.deletedAt IS NULL AND p.user.deletedAt IS NULL")
-    List<Mogakko> findOngoingMogakkosByUser(@Param("user") User user, @Param("now") LocalDateTime now);
+    @Query("SELECT m FROM Mogakko m JOIN m.participants p WHERE p.user = :user AND m.endTime >= :searchTime AND m.deletedAt IS NULL AND p.user.deletedAt IS NULL")
+    List<Mogakko> findOngoingMogakkosByUser(User user, LocalDateTime searchTime);
 
-    @Query("SELECT m FROM Mogakko m JOIN m.participants p WHERE p.user = :user AND m.endTime < :now AND m.deletedAt IS NULL AND p.user.deletedAt IS NULL")
-    List<Mogakko> findCompletedMogakkosByUser(@Param("user") User user, @Param("now") LocalDateTime now);
+    @Query("SELECT m FROM Mogakko m JOIN m.participants p WHERE p.user = :user AND m.endTime < :searchTime AND m.deletedAt IS NULL AND p.user.deletedAt IS NULL")
+    List<Mogakko> findCompletedMogakkosByUser(User user, LocalDateTime searchTime);
 
     @Modifying
     @Query("UPDATE Mogakko m SET m.views = m.views + 1 WHERE m = :mogakko")
