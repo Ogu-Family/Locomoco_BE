67d01f2eb9a462dd6fdc7f1f09b8288256bff378
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-26 01:44:04 +0900

    chore #244 : 사용하지 않는 mongoDB 관련 class 및 test 모두 삭제

--- src/main/java/org/prgms/locomocoserver/chat/domain/mongo/ChatMessageMongoRepository.java
+++
@@ -1,6 +0,0 @@
-package org.prgms.locomocoserver.chat.domain.mongo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatMessageMongoRepository extends MongoRepository<ChatMessageMongo, String> {
-}
