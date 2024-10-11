81d46494930f442325ea458660d5083c3202830e
eugene <81062608+eugene225@users.noreply.github.com>
2024-10-11 23:17:40 +0900

    refactor #256 : RefreshToken 엔터티 구성 수정

--- src/main/java/org/prgms/locomocoserver/user/domain/RefreshToken.java
+++ src/main/java/org/prgms/locomocoserver/user/domain/RefreshToken.java
@@ -7,20 +7,20 @@ import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
-@RedisHash(value = "refresh_token", timeToLive = 60*60*24*3)
+@RedisHash(value = "refresh_token", timeToLive = RefreshToken.TOKEN_TTL)
 public class RefreshToken {
 
+    public static final long TOKEN_TTL = 60 * 60 * 24 * 3; // 3일
+
     @Id
-    private String id;
+    private String refreshToken;
 
     @Indexed
     private String accessToken;
 
-    private String refreshToken;
-
     @Builder
-    public RefreshToken(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
+    public RefreshToken(String refreshToken, String accessToken) {
         this.refreshToken = refreshToken;
+        this.accessToken = accessToken;
     }
 }
