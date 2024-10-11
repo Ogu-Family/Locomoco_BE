95e0342a00aad6e82224116aeaaaaec4bed18101
eugene <81062608+eugene225@users.noreply.github.com>
2024-10-11 16:38:25 +0900

    chore #251 : 사용하지 않는 메서드 삭제

--- src/main/java/org/prgms/locomocoserver/chat/domain/ChatParticipantRepository.java
+++ src/main/java/org/prgms/locomocoserver/chat/domain/ChatParticipantRepository.java
@@ -3,7 +3,4 @@ package org.prgms.locomocoserver.chat.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
-    void deleteAllByChatRoom(ChatRoom chatRoom);
-    void deleteByChatRoomIdAndUserId(Long chatRoomId, Long userId);
-    void deleteAllByUserId(Long userId);
 }
