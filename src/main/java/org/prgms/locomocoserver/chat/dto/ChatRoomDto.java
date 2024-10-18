bfdb9dd93f5a1c741a74fb4e219fb71dcbdd380e
eugene <81062608+eugene225@users.noreply.github.com>
2024-10-18 19:17:32 +0900

    refactor #258 : ChatRoomDto 반환 형태 메시지 정보와 유저 정보 객체 분리

--- src/main/java/org/prgms/locomocoserver/chat/dto/ChatRoomDto.java
+++ src/main/java/org/prgms/locomocoserver/chat/dto/ChatRoomDto.java
@@ -12,9 +12,10 @@ public record ChatRoomDto(
         int unReadMsgCnt,
         LocalDateTime createdAt,
         LocalDateTime updatedAt,
-        ChatMessageDto lastMessage
+        ChatMessageBriefDto lastMessage,
+        ChatUserInfo userInfo
 ) {
-    public static ChatRoomDto of(ChatRoom chatRoom, int unReadMsgCnt, ChatMessageDto lastMessage) {
-        return new ChatRoomDto(chatRoom.getId(), chatRoom.getMogakko().getId(), chatRoom.getName(), chatRoom.getChatParticipants().size(), unReadMsgCnt, chatRoom.getCreatedAt(), chatRoom.getUpdatedAt(), lastMessage);
+    public static ChatRoomDto of(ChatRoom chatRoom, int unReadMsgCnt, ChatMessageBriefDto lastMessage, ChatUserInfo chatUserInfo) {
+        return new ChatRoomDto(chatRoom.getId(), chatRoom.getMogakko().getId(), chatRoom.getName(), chatRoom.getChatParticipants().size(), unReadMsgCnt, chatRoom.getCreatedAt(), chatRoom.getUpdatedAt(), lastMessage, chatUserInfo);
     }
 }
