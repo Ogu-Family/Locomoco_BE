61f8987c17c31fcf80ac3fd7eebc1148080fd145
eugene <81062608+eugene225@users.noreply.github.com>
2024-10-18 19:24:48 +0900

    test #258 : ChatRoomDto 변형된 형태 코드 수정

--- src/test/java/org/prgms/locomocoserver/chat/repository/ChatRoomRepositoryTest.java
+++ src/test/java/org/prgms/locomocoserver/chat/repository/ChatRoomRepositoryTest.java
@@ -6,8 +6,9 @@ import org.prgms.locomocoserver.chat.domain.ChatParticipant;
 import org.prgms.locomocoserver.chat.domain.ChatParticipantRepository;
 import org.prgms.locomocoserver.chat.domain.ChatRoom;
 import org.prgms.locomocoserver.chat.domain.ChatRoomRepository;
-import org.prgms.locomocoserver.chat.dto.ChatRoomDto;
 import org.prgms.locomocoserver.chat.domain.querydsl.ChatRoomCustomRepository;
+import org.prgms.locomocoserver.chat.dto.ChatRoomDto;
+import org.prgms.locomocoserver.chat.dto.ChatUserInfo;
 import org.prgms.locomocoserver.global.TestFactory;
 import org.prgms.locomocoserver.image.domain.ImageRepository;
 import org.prgms.locomocoserver.mogakkos.domain.Mogakko;
@@ -85,7 +86,7 @@ public class ChatRoomRepositoryTest {
         List<ChatRoom> chatRooms = chatRoomRepository.findByParticipantsId(user1.getId(), Long.MAX_VALUE, 10);
         // fetch join X, Transaction X, Lazy Loading
         assertThrows(LazyInitializationException.class, () -> {
-            ChatRoomDto.of(chatRooms.get(0), 0, null);
+            ChatRoomDto.of(chatRooms.get(0), 0, null, ChatUserInfo.of(user1));
         });
 
         // then
@@ -100,7 +101,7 @@ public class ChatRoomRepositoryTest {
 
         // when
         List<ChatRoom> chatRooms = chatRoomCustomRepository.findByParticipantsId(user1.getId(), LocalDateTime.now().toString(), 10);
-        ChatRoomDto.of(chatRooms.get(0), 0,null);
+        ChatRoomDto.of(chatRooms.get(0), 0, null, ChatUserInfo.of(user1));
 
         // then
         System.out.println("Participants Num : " + chatRooms.size());
