d2f541df413fee49f6e618c1e0051088b7680bfe
eugene <81062608+eugene225@users.noreply.github.com>
2024-10-25 22:55:25 +0900

    feat #258 : unReadMsg count 증가 api 추가

--- src/main/java/org/prgms/locomocoserver/chat/presentation/ChatActivityController.java
+++ src/main/java/org/prgms/locomocoserver/chat/presentation/ChatActivityController.java
@@ -2,7 +2,10 @@ package org.prgms.locomocoserver.chat.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.prgms.locomocoserver.chat.application.ChatActivityService;
+import org.prgms.locomocoserver.chat.dto.ChatActivityDto;
 import org.prgms.locomocoserver.chat.dto.request.ChatActivityRequestDto;
+import org.prgms.locomocoserver.global.annotation.GetUser;
+import org.prgms.locomocoserver.user.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +21,13 @@ public class ChatActivityController {
                                                       @RequestBody ChatActivityRequestDto request) {
         chatActivityService.updateLastReadMessage(chatRoomId, request);
         return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/chats/activity/{chatRoomId}/increase-count")
+    public ResponseEntity<ChatActivityDto> increaseUnReadMsgCnt(@PathVariable Long chatRoomId,
+                                                      @GetUser User user) {
+        ChatActivityDto chatActivityDto = chatActivityService.increaseLastReadMessage(chatRoomId, user.getId());
+        return ResponseEntity.ok(chatActivityDto);
     }
 }
 
